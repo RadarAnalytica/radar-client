88dbee54e626ccf5063f3dd346f91cc41790eeb9
radar-vladimir <radar@dmitriev.com.ru>
2024-05-23 05:05:24 +0000

    Save changes on server

--- src/pages/BlockPage.jsx
+++ src/pages/BlockPage.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import './styles.css'
 import Footer from '../components/Footer'
-import toptg from './images/toptgicon.png'
-import demo from '../assets/ext-demo.png'
-import ExtDemoSvg from '../components/ExtDemoSvg'
-import Fragment1 from './images/Fragment1'
-import YellowBtn from './images/YellowBtn'
-import Button from './images/Button'
-import Free from './images/Free'
 import TopTgIcon from './images/TopTgIcon'
 import { useNavigate } from 'react-router-dom'
 
-const Spasibo = () => {
+const BlockPage = () => {
 
     const navigate = useNavigate()
 
     return (
         <div className='spasibo-page'>
-            <div className="container spasibo-container">
+            <div className="container block-page-container">
                 <div>
                     <div className='sps-top'>
                         <svg width="200" height="74" viewBox="0 0 200 74" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -53,7 +46,7 @@ const Spasibo = () => {
                                 </a>
                             </div>
                             <div className="col top-right">
-                                <a href="https://marketplace-radar.ru/" className='sps-lk'>
+                                <span>
                                     <svg className="me-3" width="54" height="51" viewBox="0 0 54 51" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <rect width="54" height="51" rx="15" fill="#F8F8F8" />
                                         <path d="M20 32.5L34 18.5M34 18.5H20M34 18.5V32.5" stroke="#5329FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -64,7 +57,7 @@ const Spasibo = () => {
                                             <path d="M0.52 5.577V4.065H11.005L8.413 1.473L9.331 0.537L13.624 4.83L9.331 9.123L8.413 8.187L11.023 5.577H0.52Z" fill="white" fillOpacity="1" />
                                         </svg>
                                     </span>
-                                </a>
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -76,154 +69,25 @@ const Spasibo = () => {
                                 <path d="M13 26.5L21.7929 35.2929C22.1834 35.6834 22.8166 35.6834 23.2071 35.2929L40.5 18" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
                             </svg>
                             <h2 className="fw-bold mt-2">
-                                Расширение установлено!
+                                Все готово!
                             </h2>
                         </div>
                         <div className='second-sub'>
                             <h2 className='fw-bold'>
                                 <span style={{ color: 'rgba(83, 41, 255, 1)' }}>Переходите</span> на любую страницу товара на Wildberries и изучайте данные
                             </h2>
-                            <a href="https://www.wildberries.ru/catalog/154750075/detail.aspx" className="prime-btn sps__wb-link" style={{ display: 'inline-block', paddingBlock: '20px', minHeight: '63px', width: '100%', textDecoration: 'none', fontSize: '18px', textAlign: 'center' }}>
+                            <a href="https://www.wildberries.ru/catalog/211645637/detail.aspx" className="prime-btn sps__wb-link" style={{ display: 'inline-block', paddingBlock: '20px', minHeight: '63px', width: '100%', textDecoration: 'none', fontSize: '18px', textAlign: 'center' }}>
                                 Протестировать расширение
                             </a>
                         </div>
                     </div>
 
-                    <div className="sps-demo">
-                        <ExtDemoSvg />
-                    </div>
-
-                    <div className="sps-share-cta">
-                        <div className="d-flex align-items-center justify-content-between mb-2">
-                            <Fragment1 />
-                            <a href="https://radar-analytica.ru/signin">
-                                <svg className="me-3" width="54" height="51" viewBox="0 0 54 51" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <rect width="54" height="51" rx="15" fill="#F8F8F8" />
-                                    <path d="M20 32.5L34 18.5M34 18.5H20M34 18.5V32.5" stroke="#5329FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                </svg>
-                            </a>
-                        </div>
-                        <div>
-                            <h2>
-                                <span>
-                                    <YellowBtn />
-                                </span>
-                            </h2>
-                        </div>
-                        <Button />
-                    </div>
-
-                    <div className="separator">
-                        <svg width="80" height="116" viewBox="0 0 80 116" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M40 100L40 20" stroke="#F0AD00" stroke-width="2" stroke-linecap="round" />
-                            <path d="M40 99L40 84" stroke="#F0AD00" stroke-width="2" stroke-linecap="round" />
-                            <path d="M32 92L40 100L48 92" stroke="#F0AD00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            <rect opacity="0.4" x="20" width="40" height="40" rx="20" fill="#FFDB7E" />
-                            <rect x="21" y="1" width="38" height="38" rx="19" stroke="#FFDB7E" stroke-width="2" />
-                            <circle cx="40" cy="20" r="4.5" fill="white" stroke="#F0AD00" stroke-width="3" />
-                        </svg>
-
-                    </div>
-
-                    <div className="sps-bottom">
-                        <div className="bottom-first">
-                            <h6 className="fw-bold" style={{ fontSize: '34px' }}>
-                                Используйте возможности Радара на 100% и зарабатывайте больше
-                            </h6>
-                            <p style={{ fontSize: '20px' }}>
-                                Внешняя аналитика, конкурентный анализ, внутренняя аналитика по Api-ключу, мощные инструменты SEO-продвижения
-                            </p>
-                        </div>
-                        <div className="bottom-second">
-                            <div className='grey-block-btm'>
-                                <div className="col-8">
-                                    <Free />
-                                    <h3 className="fw-bold">Загляните в личный кабинет Радара</h3>
-                                    <h6 className="fw-bold">
-                                        Мощные инструменты внутренней аналитики для увеличения прибыли на маркетплейсах
-                                    </h6>
-                                </div>
-                                <div className="col-4 nav-to-profile">
-                                    <a href="https://radar-analytica.ru/signin" style={{ textDecoration: 'none', color: 'black' }}>
-                                        <div className="d-flex justify-content-end">
-                                            <svg className="me-3" width="54" height="51" viewBox="0 0 54 51" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <rect width="54" height="51" rx="15" fill="#F8F8F8" />
-                                                <path d="M20 32.5L34 18.5M34 18.5H20M34 18.5V32.5" stroke="#5329FF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                            </svg>
-                                        </div>
-                                        <br />
-                                        <div className="text-center">
-                                            <span style={{ color: 'white' }}>
-                                                Перейти в личный кабинет
-                                                <span className='ms-2'>
-                                                    <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M0.52 5.577V4.065H11.005L8.413 1.473L9.331 0.537L13.624 4.83L9.331 9.123L8.413 8.187L11.023 5.577H0.52Z" fill="white" fillOpacity="1" />
-                                                    </svg>
-                                                </span>
-                                            </span>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className='bottom-last mb-5'>
-                                <h3 className="fw-bold">А для удобства - переходите в наш бесплатный бот</h3>
-                                <p style={{ fontWeight: '600' }}>Вы сможете видеть всю статистику по магазину прямо в телефоне</p>
-                                <div className="white-last">
-                                    <div className='d-flex align-items-center gap-1'>
-                                        <img src={toptg} alt="" className='me-3' />
-                                        <span className='me-2 fw-bold' style={{ color: 'black' }}>Протестировать Telegram-бота</span>
-                                    </div>
-                                    <a href="https://t.me/SpyRadar_bot" style={{ textDecoration: 'none', color: 'black' }}>
-                                        <h1 style={{ color: 'rgba(64, 146, 247, 1)' }}>
-                                            @WB-Radar
-                                            <span className='ms-2'>
-                                                <svg width="28" height="20" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                    <path d="M0.52 5.577V4.065H11.005L8.413 1.473L9.331 0.537L13.624 4.83L9.331 9.123L8.413 8.187L11.023 5.577H0.52Z" fill="rgba(64, 146, 247, 1)" fillOpacity="1" />
-                                                </svg>
-                                            </span>
-                                        </h1>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                    
                 </div>
             </div>
-            <div className="pt-4">
-                <div className='footer-sps'>
-                    <div className="container footer-container">
-                        <div className="row">
-                            <div className="col-5 pt-1">
-                                <div className='mb-3' style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
-                                    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M24 34.6667V11.5555H30.7046C33.3307 11.5555 35.1582 11.6766 36.1872 11.9186C37.2269 12.1502 38.1219 12.5448 38.8722 13.1026C39.719 13.734 40.3675 14.5391 40.8177 15.5179C41.2786 16.4966 41.509 17.5754 41.509 18.7541C41.509 20.5432 41.0589 22.0008 40.1585 23.1269C39.2688 24.2424 37.9665 24.9844 36.2515 25.3527L42.6667 34.6667H35.4154L30.0132 25.6211V34.6667H24ZM30.0132 22.4796H31.203C32.5857 22.4796 33.5933 22.2481 34.2257 21.785C34.8688 21.322 35.1904 20.5958 35.1904 19.6065C35.1904 18.4489 34.8902 17.628 34.29 17.1439C33.7004 16.6492 32.7036 16.4019 31.2995 16.4019H30.0132V22.4796Z" fill="#1A1A1A" />
-                                        <path d="M17.0918 39.0179C15.9673 39.3478 14.7616 38.7756 14.4473 37.7269C11.7541 28.7389 11.7773 19.2416 14.5145 10.2648C14.8339 9.21748 16.0424 8.65034 17.1652 8.9849C18.288 9.31946 18.8995 10.429 18.5838 11.4773C16.1162 19.6707 16.095 28.3278 18.5225 36.5313C18.833 37.5809 18.2162 38.688 17.0918 39.0179Z" fill="#F0AD00" />
-                                        <path d="M8.07128 34.617C6.90049 34.8509 5.7234 34.2364 5.49284 33.2355C4.15877 27.4445 4.09722 21.4942 5.31128 15.6843C5.5211 14.6802 6.68529 14.0483 7.86074 14.2647C9.03618 14.4811 9.7774 15.4639 9.57139 16.4686C8.4905 21.7398 8.5463 27.1337 9.73607 32.3881C9.96284 33.3895 9.24207 34.3832 8.07128 34.617Z" fill="#F0AD00" />
-                                    </svg>
-                                    <span style={{ fontSize: '12px', fontWeight: 'bold' }}>
-                                        Комплексный инструмент для роста продаж на маркетплейсах
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="col pt-3 footer-col">
-                                <a href="https://t.me/SpyRadar_bot" style={{ textDecoration: 'none', color: 'black' }} className='fw-bold'>Telegram-бот</a>
-
-                            </div>
-                            <div className="col pt-3 footer-col">
-                                <a href="#" style={{ textDecoration: 'none', color: 'black' }} className='fw-bold'>Контакты</a>
-
-                            </div>
-                            <div className="col-3 pt-3 footer-col">
-                                <a href='/politics' className="fw-bold" style={{ textDecoration: 'none', color: 'black' }}>Политика конфиденцильности</a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Footer />
         </div>
     )
 }
 
-export default Spasibo
+export default BlockPage
