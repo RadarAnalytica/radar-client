356f58d48be6e8cb52ffd1a3d0f1f716582e753a
dmitr-1 <Vzhigd@yandex.ru>
2024-05-30 00:31:03 +0700

    mainpage

---
+++ src/pages/images/BlockImg.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import blockMainPage from './blockMainPage.png';
+
+const BlockImg = () => {
+  return (
+    <img
+      src={blockMainPage}
+      alt='Block Main Page'
+      style={{ width: '841px', height: '575' }}
+    />
+  );
+};
+
+export default BlockImg;
