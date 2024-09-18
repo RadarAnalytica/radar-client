25d11387a536b0bcdc5543676f9ea5ba815d1503
Zakar Zakaryan <zakarz1234@gmail.com>
2024-09-18 21:24:58 +0400

    Merge branch 'main' into dev
    * main:
      Added CategoryMonitoring Table Added RequestMonitoring Table Fixed StockAnalysisGlitter Tabs
      Fix initial api call AbcAnalysis
      Remove sortArrows
      Fix input fluid for smaller screens
      Fix Abc api calls
      Fixes
      fix: Fix some names in Stock analysis
      Fixed Accordion Header text
      Fixed OrdersMap colors of ToolTips in Safari browser
      Fixes
      Add refreshToken listener ecery 1 minutes if user exists in MainPage and TariffsPage
      Fix changing days and period on Dashboard, OrdersMap and AbcAnalysisPage
      Add fetchMessages with first render
      Fix routing
      Add NoSubscriptionPage Onboarding
      Refactor
      Add routes logic
      add loader to all plates when no data on dashboard page
    
    # Conflicts:
    #	src/App.css
    #	src/components/StockAnalysisGlitter.jsx
    #	src/pages/OrdersMap.jsx
    #	src/pages/StockAnalysis.jsx

--- src/components/StockAnalysisGlitter.jsx
+++ src/components/StockAnalysisGlitter.jsx
@@ -17,339 +17,111 @@ import { fetchStockAnalysisData } from '../redux/stockAnalysis/stockAnalysisData
 import AuthContext from '../service/AuthContext';
 
 const StockAnalysisGlitter = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const { authToken } = useContext(AuthContext);
-  const shops = useAppSelector((state) => state.shopsSlice.shops);
 
-  const [activeTab, setActiveTab] = useState('summary');
-  const [days, setDays] = useState(30);
-  const [productData, setProductData] = useState({});
-  const [isInitialLoading, setIsInitialLoading] = useState(true);
-  const productId = id;
 
-  const linkToWb = `https://www.wildberries.ru/catalog/${productId}/detail.aspx`;
+    const [activeTab, setActiveTab] = useState('summary');
+    const [days, setDays] = useState(30);
 
-  const productBySku = Array.isArray(productData)
-    ? productData.find((item) => item.sku === id)
-    : null;
 
-  const storedActiveShop = localStorage.getItem('activeShop');
-  let activeShop;
-  if (storedActiveShop && typeof storedActiveShop === 'string') {
-    try {
-      activeShop = JSON.parse(storedActiveShop);
-    } catch (error) {
-      console.error('Error parsing storedActiveShop:', error);
-      activeShop = null;
-    }
-  }
-  const activeShopId = activeShop?.id;
-  const idShopAsValue =
-    activeShopId != undefined ? activeShopId : shops?.[0]?.id;
-  const [activeBrand, setActiveBrand] = useState(idShopAsValue);
-
-  function TabContent() {
-    switch (activeTab) {
-      case 'summary':
-        return (
-          <Summary
-            days={days}
-            productBySku={productBySku}
-            isInitialLoading={isInitialLoading}
-          />
-        );
-      case 'product':
-        return (
-          <Product
-            productBySku={productBySku}
-            isInitialLoading={isInitialLoading}
-          />
-        );
-      case 'categoryMonitoring':
-        return <CategoryMonitoring />;
-      case 'requestMonitoring':
-        return <RequestMonitoring />;
-      default:
-        return ''; // Компонент по умолчанию, если ни одна вкладка не выбрана
-    }
-  }
-
-  useEffect(() => {
-    dispatch(fetchStockAnalysisData({ authToken, days, activeBrand })).then(
-      (response) => {
-        if (response.payload) {
-          setProductData(response.payload);
-          setIsInitialLoading(false);
+    function TabContent() {
+        switch (activeTab) {
+            case 'summary':
+                return <Summary days={days} />;
+            case 'product':
+                return <Product />;
+            case 'categoryMonitoring':
+                return <CategoryMonitoring />;
+            case 'requestMonitoring':
+                return <RequestMonitoring />;
+            default:
+                return ""; // Компонент по умолчанию, если ни одна вкладка не выбрана
         }
-      }
-    );
-  }, []);
+    }
 
-  useEffect(() => {
-    setIsInitialLoading(true);
-    dispatch(fetchStockAnalysisData({ authToken, days, activeBrand })).then(
-      (response) => {
-        if (response.payload) {
-          setProductData(response.payload);
-          setIsInitialLoading(false);
-        }
-      }
-    );
-  }, [activeBrand, days]);
 
-  return (
-    <div className='dashboard-page'>
-      <SideNav />
-      <div className='dashboard-content pb-3'>
-        <TopNav title={'Товарная аналитика'} />
-        <div
-          className='container dash-container pt-0 d-flex gap-3'
-          style={{ justifyContent: 'space-between', marginBottom: '9px' }}
-        >
-          <div className='productInfo-price-photo'>
-            {isInitialLoading && (
-              <div
-                className='d-flex flex-column align-items-center justify-content-center'
-                style={{ height: '100%', paddingTop: '5%', width: '100%' }}
-              >
-                <span className='loader'></span>
-              </div>
-            )}
-            {!isInitialLoading && (
-              <>
-                <div className='productInfo-price-photo-photo'>
-                  <img src={productBySku?.photo} alt='product image' />
-                </div>
-                <div className='productInfo-price-photo-price'>
-                  <p
-                    style={{
-                      color: '#8C8C8C',
-                      fontSize: '16px',
-                      fontWeight: '500',
-                      marginBottom: '0',
-                    }}
-                  >
-                    Цена
-                  </p>
-                  <span style={{ fontSize: '24px', fontWeight: '700' }}>
-                    {productBySku?.basic} ₽
-                  </span>
-                </div>
-              </>
-            )}
-          </div>
+    return (
+        <div className='dashboard-page'>
+            <SideNav />
+            <div className="dashboard-content pb-3">
+                <TopNav title={'Товарная аналитика'} />
+                <div className="container dash-container p-3 pt-0 d-flex gap-3" style={{ justifyContent: 'space-between' }}>
+                    <div style={{ display: 'flex', backgroundColor: 'white', width: '422px', height: '199px', borderRadius: '8px' }}>
+                        <div>
+                            <img style={{ marginTop: '10px' }} src={product} alt="" />
+                        </div>
+                        <div style={{ marginLeft: '10px', marginTop: '10px' }}>
+                            <p style={{ color: '#8C8C8C', fontSize: '16px', fontWeight: '500', marginBottom: '0' }}>Цена</p>
+                            <span style={{ fontSize: '24px', fontWeight: '700' }}>899,00 р</span>
+                        </div>
 
-          <div
-            style={{
-              backgroundColor: 'white',
-              width: '422px',
-              height: '199px',
-              borderRadius: '8px',
-            }}
-          >
-            {isInitialLoading && (
-              <div
-                className='d-flex flex-column align-items-center justify-content-center'
-                style={{ height: '100%', paddingTop: '5%', width: '100%' }}
-              >
-                <span className='loader'></span>
-              </div>
-            )}
-            {!isInitialLoading && (
-              <>
-                <div style={{ display: 'flex', marginTop: '10px' }}>
-                  <img
-                    style={{ marginLeft: '10px' }}
-                    src={glitterStar}
-                    alt=''
-                  />
-                  <p
-                    style={{
-                      fontSize: '24px',
-                      fontWeight: '700',
-                      marginLeft: '10px',
-                      marginBottom: '0',
-                      marginTop: '10px',
-                    }}
-                  >
-                    5.0
-                  </p>
-                  <div
-                    style={{
-                      width: '60px',
-                      height: '60px',
-                      backgroundColor: 'rgba(240, 173, 0, 0.2)',
-                      borderRadius: '10px',
-                      marginLeft: '60%',
-                    }}
-                  >
-                    <img
-                      style={{ marginLeft: '15px', marginTop: '11px' }}
-                      src={glityellow}
-                      alt=''
-                    />
-                  </div>
-                </div>
-                <p
-                  style={{
-                    color: '#8C8C8C',
-                    fontSize: '16px',
-                    fontWeight: '500',
-                    marginLeft: '10px',
-                    marginTop: '-10px',
-                  }}
-                >
-                  189 отзывов
-                </p>
-                <div style={{ marginLeft: '10px', marginTop: '20%' }}>
-                  <a
-                    href={linkToWb}
-                    style={{
-                      textDecoration: 'none',
-                      fontSize: '16px',
-                      fontWeight: '600',
-                    }}
-                  >
-                    Посмотреть на WB
-                  </a>
-                </div>
-              </>
-            )}
-          </div>
 
-          <div className='barcode-sku-brand'>
-            {isInitialLoading && (
-              <div
-                className='d-flex flex-column align-items-center justify-content-center'
-                style={{ height: '100%', paddingTop: '5%', width: '100%' }}
-              >
-                <span className='loader'></span>
-              </div>
-            )}
-            {!isInitialLoading && (
-              <div className='barcode-wrapper'>
-                <div className='barcode'>
-                  <div className='barcode-row'>
-                    <p className='barcode-text-title'>SKU</p>
-                    <p className='barcode-text'>{productBySku?.sku}</p>
-                  </div>
-                  <div className='barcode-row-image'>
-                    <img src={glitFile} alt='Folder image' />
-                  </div>
+                    </div>
+                    <div style={{ backgroundColor: 'white', width: '422px', height: '199px', borderRadius: '8px' }}>
+                        <div style={{ display: 'flex', marginTop: '10px' }}>
+                            <img style={{ marginLeft: '10px' }} src={glitterStar} alt="" />
+                            <p style={{ fontSize: '24px', fontWeight: '700', marginLeft: '10px', marginBottom: '0', marginTop: '10px' }} >5.0</p>
+                            <div style={{ width: '60px', height: '60px', backgroundColor: 'rgba(240, 173, 0, 0.2)', borderRadius: '10px', marginLeft: '60%' }}>
+                                <img style={{ marginLeft: '15px', marginTop: '11px' }} src={glityellow} alt="" />
+                            </div>
+                        </div>
+                        <p style={{ color: '#8C8C8C', fontSize: '16px', fontWeight: '500', marginLeft: '10px', marginTop: '-10px' }}>189 отзывов</p>
+                        <div style={{ marginLeft: '10px', marginTop: '20%' }}>
+                            <a href='#' style={{ textDecoration: 'none', fontSize: '16px', fontWeight: '600' }}>Посмотрреть на  WB</a>
+                        </div>
+                    </div>
+                    <div style={{ backgroundColor: 'white', width: '422px', height: '199px', borderRadius: '8px' }}>
+                        <div style={{ marginTop: '5px', marginLeft: '15px' }}>
+                            <div style={{ display: 'flex' }} >
+                                <div style={{ flexDirection: 'column' }}>
+                                    <p style={{ marginBottom: '0', fontSize: '16px', fontWeight: '500', color: '#8C8C8C' }}>Баркод</p>
+                                    <p>2000662320049</p>
+                                </div>
+                                <div style={{ width: '60px', height: '60px', backgroundColor: 'rgba(126, 126, 126, 0.2)', borderRadius: '10px', marginLeft: '47%', marginTop: '1%' }}>
+                                    <img style={{ marginLeft: '13px', marginTop: '15px' }} src={glitFile} alt="" />
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                <p style={{ marginBottom: '0', fontSize: '16px', fontWeight: '500', color: '#8C8C8C' }}>SKU</p>
+                                <p>16367820</p>
+                            </div>
+                            <div>
+                                <p style={{ marginBottom: '0', fontSize: '16px', fontWeight: '500', color: '#8C8C8C' }}>Бренд</p>
+                                <p>Название бренда</p>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <div>
-                  <p
-                    style={{
-                      marginBottom: '0',
-                      fontSize: '16px',
-                      fontWeight: '500',
-                      color: '#8C8C8C',
-                    }}
-                  >
-                    Бренд
-                  </p>
-                  <p>{productBySku?.brandName}</p>
+                {/* <p style={{marginLeft: '45px', fontSize: '16px', fontWeight: '500'}}>Период</p> */}
+                <div className='filter-tab-container' style={{ marginLeft: '50px' }}>
+                    <div>
+                        <StockAnalysisGlitterFilter setDays={setDays} />
+                    </div>
+
+                    <div className="stock-analysis-tab-container" >
+                        <div onClick={() => setActiveTab('summary')} className='stock-analysis-tab-container-item' style={{
+                            backgroundColor: activeTab === 'summary' ? '#5329FF1A' : 'transparent'
+                        }}>
+                            <p style={{ textAlign: 'center' }
+                            } > Сводка</p>
+                        </div>
+                        <div onClick={() => setActiveTab('product')} className='stock-analysis-tab-container-item' style={{ backgroundColor: activeTab === 'product' ? '#5329FF1A' : 'transparent' }}>
+                            <p style={{ textAlign: 'center' }}>О продукте</p>
+                        </div>
+                        <div onClick={() => setActiveTab('categoryMonitoring')} className='stock-analysis-tab-container-item' style={{ cursor: 'pointer', backgroundColor: activeTab === 'categoryMonitoring' ? '#5329FF1A' : 'transparent' }}>
+                            <p style={{ textAlign: 'center' }} >Мониторинг категорий</p>
+                        </div>
+                        <div onClick={() => setActiveTab('requestMonitoring')} className='stock-analysis-tab-container-item' style={{ cursor: 'pointer', backgroundColor: activeTab === 'requestMonitoring' ? '#5329FF1A' : 'transparent' }}>
+                            <p style={{ textAlign: 'center' }}>Монтроринг запросов</p>
+                        </div>
+                    </div>
+
                 </div>
-              </div>
-            )}
-          </div>
-        </div>
-        {/* <p style={{marginLeft: '45px', fontSize: '16px', fontWeight: '500'}}>Период</p> */}
-        <div
-          className='container dash-container pt-0 p-3 d-flex'
-          style={{marginBottom: '4px'}}
-        >
-          <StockAnalysisGlitterFilter setDays={setDays} />
-          <div className='productInfo-nav'>
-            <div
-              onClick={() => setActiveTab('summary')}
-              style={{
-                height: '43px',
-                borderRadius: '8px',
-                cursor: 'pointer',
-                backgroundColor:
-                  activeTab === 'summary' ? '#5329FF1A' : 'transparent',
-                color:
-                  activeTab === 'summary' ? 'rgba(26, 26, 26, 1)' : '#8C8C8C',
-              }}
-            >
-              <p
-                className='productInfo-nav-text'
-                style={{
-                  fontWeight: activeTab === 'summary' ? '600' : '500',
-                }}
-              >
-                Сводка
-              </p>
-            </div>
-            <div
-              onClick={() => setActiveTab('product')}
-              style={{
-                height: '43px',
-                borderRadius: '8px',
-                cursor: 'pointer',
-                backgroundColor:
-                  activeTab === 'product' ? '#5329FF1A' : 'transparent',
-                color:
-                  activeTab === 'product' ? 'rgba(26, 26, 26, 1)' : '#8C8C8C',
-              }}
-            >
-              <p 
-              className='productInfo-nav-text'
-              style={{
-                fontWeight: activeTab === 'product' ? '600' : '500',
-              }}
-              >
-                О продукте
-              </p>
-            </div>
-            <div
-              onClick={() => setActiveTab('categoryMonitoring')}
-              style={{
-                height: '43px',
-                borderRadius: '8px',
-                cursor: 'pointer',
-                backgroundColor:
-                  activeTab === 'categoryMonitoring'
-                    ? '#5329FF1A'
-                    : 'transparent',
-                color:
-                  activeTab === 'categoryMonitoring' ? 'rgba(26, 26, 26, 1)' : '#8C8C8C',
-              }}
-            >
-              <p className='productInfo-nav-text' style={{
-                fontWeight: activeTab === 'categoryMonitoring' ? '600' : '500',
-              }}>
-                Мониторинг категорий
-              </p>
-            </div>
-            <div
-              onClick={() => setActiveTab('requestMonitoring')}
-              style={{
-                height: '43px',
-                borderRadius: '8px',
-                cursor: 'pointer',
-                backgroundColor:
-                  activeTab === 'requestMonitoring'
-                    ? '#5329FF1A'
-                    : 'transparent',
-                color:
-                  activeTab === 'requestMonitoring' ? 'rgba(26, 26, 26, 1)' : '#8C8C8C',
-              }}
-            >
-              <p className='productInfo-nav-text' style={{
-                fontWeight: activeTab === 'requestMonitoring' ? '600' : '500',
-              }}>
-                Монтроринг запросов
-              </p>
-            </div>
-          </div>
-        </div>
-        {TabContent()}
-      </div>
-    </div>
-  );
-};
+                {TabContent()}
+
+            </div >
+        </div >
+    )
+}
 
 export default StockAnalysisGlitter;
