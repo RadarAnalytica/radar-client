ab4d4deeae11a4888cc20b48245546293304f32e
Zakar Zakaryan <zakarz1234@gmail.com>
2024-09-18 21:30:12 +0400

    Fixed stockanalysis page

--- src/pages/StockAnalysis.jsx
+++ src/pages/StockAnalysis.jsx
@@ -15,7 +15,7 @@ import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { fetchShops } from '../redux/shops/shopsActions';
 import DragDropFile from '../components/DragAndDropFiles';
 import Modal from 'react-bootstrap/Modal';
-import {fetchStockAnalysisData} from '../redux/stockAnalysis/stockAnalysisDataActions';
+import { fetchStockAnalysisData } from '../redux/stockAnalysis/stockAnalysisDataActions';
 import { ServiceFunctions } from "../service/serviceFunctions";
 import DownloadButton from '../components/DownloadButton';
 import NoSubscriptionPage from "./NoSubscriptionPage";
@@ -68,12 +68,12 @@ const StockAnalysis = () => {
     is_primary_collect: allShop,
     is_valid: true,
   };
- 
+
   const shouldDisplay = activeShop
-  ? activeShop.is_primary_collect
-  : oneShop
-  ? oneShop.is_primary_collect
-  : allShop;
+    ? activeShop.is_primary_collect
+    : oneShop
+      ? oneShop.is_primary_collect
+      : allShop;
 
   const handleCostPriceShow = () => {
     setCostPriceShow(true);
@@ -101,7 +101,7 @@ const StockAnalysis = () => {
 
   const filterData = (data, query) => {
     if (!query) return data;
-    return data.filter(item => 
+    return data.filter(item =>
       item?.sku?.toLowerCase().includes(query.toLowerCase()) ||
       item?.vendorСode?.toLowerCase().includes(query.toLowerCase()) ||
       item?.productName?.toLowerCase().includes(query.toLowerCase())
@@ -155,9 +155,9 @@ const StockAnalysis = () => {
   }, [shops]);
 
   useEffect(() => {
-    if (shops?.length === 0 && !isInitialLoading ) {
+    if (shops?.length === 0 && !isInitialLoading) {
       navigate("/onboarding");
-    } 
+    }
   }, [isInitialLoading, shops.length]);
 
 
@@ -172,7 +172,7 @@ const StockAnalysis = () => {
     setActiveBrand(shopId);
   };
 
-  
+
   const handleUpdateDashboard = () => {
     setTimeout(() => {
       updateDataDashBoardCaller();
@@ -198,15 +198,13 @@ const StockAnalysis = () => {
         <SideNav />
         <div className='dashboard-content pb-3'>
           <TopNav title={'Товарная аналитика'} />
-          {!isInitialLoading &&
-          !hasSelfCostPrice &&
-          activeShopId !== 0 &&
-          shouldDisplay ? (
+          {!isInitialLoading && !hasSelfCostPrice && activeShopId !== 0 && shouldDisplay ? (
             <SelfCostWarning
               activeBrand={activeBrand}
               onUpdateDashboard={handleUpdateDashboard}
             />
           ) : null}
+
           <div className='pt-0 d-flex gap-3'>
             <StockAnalysisFilter
               shops={shops}
@@ -215,10 +213,10 @@ const StockAnalysis = () => {
               activeShopId={activeShopId}
             />
           </div>
+
           {shouldDisplay ? (
             <>
-              <div 
-              className='input-and-button-container container dash-container p-3 pb-4 pt-0 d-flex flex-wrap justify-content-between align-items-center'>
+              <div className='input-and-button-container container dash-container p-3 pb-4 pt-0 d-flex flex-wrap justify-content-between align-items-center'>
                 <div className='search search-container'>
                   <div className='search-box'>
                     <input
@@ -230,11 +228,7 @@ const StockAnalysis = () => {
                     />
                     <button className='search-box-btn'>
                       <img
-                        onClick={() =>
-                          setDataTable(
-                            filterData(stockAnalysisData, searchQuery)
-                          )
-                        }
+                        onClick={() => setDataTable(filterData(stockAnalysisData, searchQuery))}
                         style={{ marginLeft: "10px", cursor: "pointer" }}
                         src={SearchButton}
                         alt='search'
@@ -242,35 +236,30 @@ const StockAnalysis = () => {
                     </button>
                   </div>
                 </div>
+
                 <div className='button-container d-flex gap-3'>
-                  <div
-                    className='d-flex'
-                    style={{
-                      gap: '20px',
-                      alignItems: 'center',
-                    }}
-                  >
+                  <div className='d-flex' style={{ gap: '20px', alignItems: 'center' }}>
                     <div className='button-container d-flex gap-3'>
-                    <div>
-                      <img
-                        style={{ cursor: 'pointer' }}
-                        onClick={handleCostPriceShow}
-                        src={StockCostPrice}
-                        alt=''
-                      />
-                    </div>
-                    <div>
-                      <DownloadButton
-                        handleDownload={() =>
-                          getFileClickHandler(authToken, activeBrand)
-                        }
-                      />
+                      <div>
+                        <img
+                          style={{ cursor: 'pointer' }}
+                          onClick={handleCostPriceShow}
+                          src={StockCostPrice}
+                          alt=''
+                        />
+                      </div>
+                      <div>
+                        <DownloadButton
+                          handleDownload={() => getFileClickHandler(authToken, activeBrand)}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
+
               <div style={{ height: '20px' }}></div>
-              <TableStock dataTable={dataTable} setDataTable={setDataTable} />          
+              <TableStock dataTable={dataTable} setDataTable={setDataTable} />
             </>
           ) : (
             <DataCollectionNotification
@@ -279,6 +268,8 @@ const StockAnalysis = () => {
           )}
         </div>
       </div>
+
+      {/* Modal for Cost Price */}
       <Modal
         show={costPriceShow}
         onHide={handleCostPriceClose}
@@ -308,7 +299,7 @@ const StockAnalysis = () => {
                     <path
                       d='M14 21.75H3C1.75736 21.75 0.75 20.7426 0.75 19.5V3.5C0.75 2.25736 1.75736 1.25 3 1.25H10.8588L16.25 6.32405V19.5C16.25 20.7426 15.2426 21.75 14 21.75Z'
                       stroke='black'
-                      stroke-opacity='0.5'
+                      strokeOpacity='0.5'
                       strokeWidth='1.5'
                     />
                   </svg>
@@ -346,19 +337,6 @@ const StockAnalysis = () => {
             </div>
           ) : (
             <div className='d-flex flex-column align-items-center justify-content-around w-100'>
-              {/* <div className="file-block d-flex flex-column align-items-center justify-content-around w-100 mt-2 gap-2">
-                            <svg width="64" height="48" viewBox="0 0 64 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M20 11C17.5147 11 15.5 13.0147 15.5 15.5V16C15.5 18.4853 17.5147 20.5 20 20.5C22.4853 20.5 24.5 18.4853 24.5 16V15.5C24.5 13.0147 22.4853 11 20 11Z" fill="#5329FF" />
-                                <path d="M11.5 47H53.5C58.4706 47 62.5 42.9706 62.5 38V30L47.8422 21.4198C44.3822 19.3944 39.9996 19.902 37.0941 22.6647L26.75 32.5L11.5 47Z" fill="#5329FF" />
-                                <path d="M11.5 47H53.5C58.4706 47 62.5 42.9706 62.5 38V30M11.5 47H10C5.30558 47 1.5 43.1944 1.5 38.5V38.5M11.5 47L26.75 32.5M62.5 30V10C62.5 5.02944 58.4706 1 53.5 1H10.5C5.52944 1 1.5 5.02944 1.5 10V38.5M62.5 30L47.8422 21.4198C44.3822 19.3944 39.9996 19.902 37.0941 22.6647L26.75 32.5M26.75 32.5L21.1388 29.258C17.7739 27.3138 13.5411 27.749 10.6422 30.3373L1.5 38.5M24.5 15.5V16C24.5 18.4853 22.4853 20.5 20 20.5V20.5C17.5147 20.5 15.5 18.4853 15.5 16V15.5C15.5 13.0147 17.5147 11 20 11V11C22.4853 11 24.5 13.0147 24.5 15.5Z" stroke="#5329FF" strokeWidth="1.5" />
-                            </svg>
-                            <h5 className='fw-bold'>Перетащите файл сюда</h5>
-                            <span className='clue-text'>или нажмите на кнопку</span>
-                            <button className='prime-btn' style={{ padding: '16px 20px' }}
-                                onClick={() => setShowDelete(false)}>
-                                Выбрать файл
-                            </button>
-                        </div> */}
               <DragDropFile files={file} setFiles={setFile} />
               <div className='d-flex justify-content-center w-100 mt-2 gap-2'>
                 <a
@@ -376,5 +354,6 @@ const StockAnalysis = () => {
     </>
   );
 };
+
 
 export default StockAnalysis;
