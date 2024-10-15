9497dfdb087b57a6a922f3134b02b28dbfe0e78c
Modin Sergei <modinsv@yandex.ru>
2024-10-15 11:36:29 +0300

    Add handle click on keyword to open in new tab with search page on wb

--- src/components/SeoCompaire.jsx
+++ src/components/SeoCompaire.jsx
@@ -6,7 +6,6 @@ import SortArrows from './SortArrows';
 import VennDiagram from './VennDiagram';
 
 const SeoCompaire = ({ compaireData }) => {
-  console.log('compaireData', compaireData);
   const [byOptions, setByOptions] = useState('onlyA');
   const [searchQuery, setSearchQuery] = useState('');
   const [sortConfig, setSortConfig] = useState({ key: null, direction: 'asc' });
@@ -15,11 +14,15 @@ const SeoCompaire = ({ compaireData }) => {
   const contentA = compaireData?.products_a ?? [];
   const contentB = compaireData?.products_b ?? [];
 
-  const arrA = Object.keys(compaireData?.keywords_a).map(x => x.toLowerCase());
-  const arrB = Object.keys(compaireData?.keywords_b).map(x => x.toLowerCase());
+  const arrA = Object.keys(compaireData?.keywords_a).map((x) =>
+    x.toLowerCase()
+  );
+  const arrB = Object.keys(compaireData?.keywords_b).map((x) =>
+    x.toLowerCase()
+  );
 
-const intersection = arrA.filter(x => arrB.includes(x));
-const intersectionLength = intersection.length;
+  const intersection = arrA.filter((x) => arrB.includes(x));
+  const intersectionLength = intersection.length;
 
   const radioOptions = [
     { value: 'onlyA', label: 'Только А' },
@@ -34,11 +37,11 @@ const intersectionLength = intersection.length;
       const { direction } = sortConfig;
       const isAscending = direction === 'asc';
       const newDirection = isAscending ? 'desc' : 'asc';
-  
+
       const newSortedData = [...sortedData].sort((a, b) => {
         const aValue = key === 'key' ? a.key : a.value;
         const bValue = key === 'key' ? b.key : b.value;
-  
+
         if (typeof aValue === 'number' && typeof bValue === 'number') {
           return isAscending ? aValue - bValue : bValue - aValue;
         } else if (typeof aValue === 'string' && typeof bValue === 'string') {
@@ -49,7 +52,7 @@ const intersectionLength = intersection.length;
           return 0;
         }
       });
-  
+
       setSortConfig({ key, direction: newDirection });
       setSortedData(newSortedData);
     },
@@ -57,25 +60,38 @@ const intersectionLength = intersection.length;
   );
 
   useEffect(() => {
-    const keywordsA = Object.keys(compaireData?.keywords_a || {}).map(x => x.toLowerCase());
-    const keywordsB = Object.keys(compaireData?.keywords_b || {}).map(x => x.toLowerCase());
-    const intersection = keywordsA.filter(x => keywordsB.includes(x));
+    const keywordsA = Object.keys(compaireData?.keywords_a || {}).map((x) =>
+      x.toLowerCase()
+    );
+    const keywordsB = Object.keys(compaireData?.keywords_b || {}).map((x) =>
+      x.toLowerCase()
+    );
+    const intersection = keywordsA.filter((x) => keywordsB.includes(x));
 
     const dataMap = {
-      onlyA: Object.entries(compaireData?.keywords_a).map(([key, value]) => ({ key, value })),
-      onlyB:  Object.entries(compaireData?.keywords_b).map(([key, value]) => ({ key, value })),
-     commonAB: intersection.map(key => ({
-      key,
-      value: Math.max(compaireData?.keywords_a[key] || 0, compaireData?.keywords_b[key] || 0)
-    })),
-    differenceAB: Object.entries(compaireData?.keywords_a || {})
-      .filter(([key]) => !keywordsB.includes(key.toLowerCase()))
-      .map(([key, value]) => ({ key, value })),
-    differenceBA: Object.entries(compaireData?.keywords_b || {})
-      .filter(([key]) => !keywordsA.includes(key.toLowerCase()))
-      .map(([key, value]) => ({ key, value })),
-  };
-  setSortedData(dataMap[byOptions] || []);
+      onlyA: Object.entries(compaireData?.keywords_a).map(([key, value]) => ({
+        key,
+        value,
+      })),
+      onlyB: Object.entries(compaireData?.keywords_b).map(([key, value]) => ({
+        key,
+        value,
+      })),
+      commonAB: intersection.map((key) => ({
+        key,
+        value: Math.max(
+          compaireData?.keywords_a[key] || 0,
+          compaireData?.keywords_b[key] || 0
+        ),
+      })),
+      differenceAB: Object.entries(compaireData?.keywords_a || {})
+        .filter(([key]) => !keywordsB.includes(key.toLowerCase()))
+        .map(([key, value]) => ({ key, value })),
+      differenceBA: Object.entries(compaireData?.keywords_b || {})
+        .filter(([key]) => !keywordsA.includes(key.toLowerCase()))
+        .map(([key, value]) => ({ key, value })),
+    };
+    setSortedData(dataMap[byOptions] || []);
   }, [byOptions, compaireData]);
 
   const handleRadioChange = (value) => {
@@ -86,20 +102,30 @@ const intersectionLength = intersection.length;
     setSearchQuery(e.target.value);
   };
 
+  const handleKeywordClick = (keyword) => {
+    const url = `https://www.wildberries.ru/catalog/0/search.aspx?search=${keyword}`;
+    window.open(url, '_blank');
+  };
+
   const renderData = useMemo(() => {
     if (Array.isArray(sortedData) && sortedData.length > 0) {
       const filteredData = sortedData.filter((item) => {
         return item.key.toLowerCase().includes(searchQuery.toLowerCase());
       });
-  
+
       if (filteredData.length === 0) {
         return <div>Ничего не найдено</div>;
       }
-  
+
       return filteredData.map((item, index) => {
         return (
           <div className={styles.tableContentItem} key={index}>
-            <div>{item.key}</div>
+            <div
+              className={styles.tableContentItemKey}
+              onClick={() => handleKeywordClick(item.key)}
+            >
+              {item.key}
+            </div>
             <div>{item.value}</div>
           </div>
         );
@@ -115,19 +141,21 @@ const intersectionLength = intersection.length;
   const clickProduct = (wb_id) => {
     const url = `https://www.wildberries.ru/catalog/${wb_id}/detail.aspx`;
     window.open(url, '_blank');
-  }
+  };
 
   return (
     <div className={styles.seoCompaireWrapper}>
       <div className={styles.topBlock}>
         <div>
-        {compaireData && compaireData.keywords_a && compaireData.keywords_b && (
-          <VennDiagram
-            groupACount={Object.keys(compaireData.keywords_a).length}
-            groupBCount={Object.keys(compaireData.keywords_b).length}
-            intersectionCount={intersectionLength}
-          />
-)}
+          {compaireData &&
+            compaireData.keywords_a &&
+            compaireData.keywords_b && (
+              <VennDiagram
+                groupACount={Object.keys(compaireData.keywords_a).length}
+                groupBCount={Object.keys(compaireData.keywords_b).length}
+                intersectionCount={intersectionLength}
+              />
+            )}
         </div>
         <div className={styles.seoTableWrapper}>
           <div className={styles.seoTableHeader}>
@@ -154,7 +182,7 @@ const intersectionLength = intersection.length;
                     }}
                   />
                   <span onClick={() => clickProduct(item.wb_id)}>
-                  {item.title}
+                    {item.title}
                   </span>
                 </div>
               ))}
@@ -177,8 +205,8 @@ const intersectionLength = intersection.length;
                         'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAgAB/HHpC6UAAAAASUVORK5CYII=';
                     }}
                   />
-                   <span onClick={() => clickProduct(item.wb_id)}>
-                  {item.title}
+                  <span onClick={() => clickProduct(item.wb_id)}>
+                    {item.title}
                   </span>
                 </div>
               ))}
