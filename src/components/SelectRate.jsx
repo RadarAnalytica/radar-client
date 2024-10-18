600e76cd07f12eafac2a332efb78002ef6fe5aba
Zakar Zakaryan <zakarz1234@gmail.com>
2024-10-18 12:19:02 +0400

    Fixed space in SelectRate component

--- src/components/SelectRate.jsx
+++ src/components/SelectRate.jsx
@@ -902,8 +902,8 @@ const SelectRate = ({ redirect }) => {
                 }}
                 id='btnDop'
               >
-                {selectedPeriod === "6month" ? <Steps.Circle /> : <span></span>}
-                <span>6 месяцев</span>
+                {selectedPeriod === "6month " ? <Steps.Circle /> : <span></span>}
+                <span>6 месяцев{" "}</span>
                 <span className='saleTextMobile'>
                   {!subscriptionDiscount ? "до -60%" : "-50%"}
                 </span>
