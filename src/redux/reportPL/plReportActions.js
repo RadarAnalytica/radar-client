19cd99eb13d45fc2e9c87779423f5feb49ac4821
Modin Sergei <modinsv@yandex.ru>
2025-01-23 14:58:49 +0300

    Fix /api/report/p_l/data POST to show actual data in table on the page

--- src/redux/reportPL/plReportActions.js
+++ src/redux/reportPL/plReportActions.js
@@ -6,16 +6,27 @@ export const fetchPLReport = createAsyncThunk(
     async ({ brandFilter, groupFilter, token }) => {
         console.log('brandFilter', brandFilter);
         console.log('groupFilter', groupFilter);
+
+        // Convert comma-separated strings to arrays
+        const brandFilterArray = brandFilter.split(',');
+        const groupFilterArray = groupFilter.split(',');
+
+        const requestBody = {
+            brand_filter: brandFilterArray,
+            group_filter: groupFilterArray
+        };
         
         
         const res = await fetch(
-            `${URL}/api/report/p_l/data?brand_filter=${brandFilter}&group_filter=${groupFilter}`,
+            `${URL}/api/report/p_l/data`,
             {
-                method: 'GET',
+                method: 'POST',
                 headers: {
                     'accept': 'application/json',
+                    'Content-Type': 'application/json',
                     'Authorization': token
-                }
+                },
+                body: JSON.stringify(requestBody)
             }
         );
         return await res.json();
