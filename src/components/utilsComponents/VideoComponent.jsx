9253a7e696a83a9d06cca3d9aab6b88cfc30069c
Zakar Zakaryan <zakarz1234@gmail.com>
2025-01-27 17:33:34 +0400

    Revert "Test: Fixing issues with MainPage"
    This reverts commit 0948efc6cf50877c0a0fb4bff87377f801299d51.

--- src/components/utilsComponents/VideoComponent.jsx
+++ src/components/utilsComponents/VideoComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 
-const VideoComponent = ({ heavyVideoSrc, lightVideoSrc, preview, style }) => {
+const VideoComponent = ({ heavyVideoSrc, lightVideoSrc, style }) => {
     const videoRef = useRef(null); // Reference to the video element
     const [useHeavyVideo, setUseHeavyVideo] = useState(false);
 
@@ -27,7 +27,6 @@ const VideoComponent = ({ heavyVideoSrc, lightVideoSrc, preview, style }) => {
     return (
         <div>
             <video
-                poster={preview}
                 ref={videoRef}
                 src={useHeavyVideo ? heavyVideoSrc : lightVideoSrc}
                 style={style}
