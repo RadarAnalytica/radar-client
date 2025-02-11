6d9068060bbce19fc09436a534b111aff5124c08
Modin Sergei <modinsv@yandex.ru>
2025-02-11 10:38:04 +0300

    Fix arrows in pagination and fix loader

--- src/pages/RequestMonitoringPage.jsx
+++ src/pages/RequestMonitoringPage.jsx
@@ -36,6 +36,7 @@ const RequestMonitoringPage = () => {
     const [errorMessage, setErrorMessage] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [sort, setSort] = useState("desc");
+    const [isTableLoading, setIsTableLoading] = useState(false);
 
     // Функции для открытия и закрытия модального окна
     const handleShowModal = () => setShowModal(true);
@@ -47,7 +48,6 @@ const RequestMonitoringPage = () => {
 
     const handleFilterSearch = async () => {
         if (searchInputQuery) {
-            setIsLoading(true);
             await updateRequestMonitoring(authToken, searchInputQuery, Number(days), page, monitorData.page_limit ?? 25, sort)
 
         } else {
@@ -63,7 +63,7 @@ const RequestMonitoringPage = () => {
     const updateRequestMonitoring = async (
         token, product, period, page, page_limit, sort
     ) => {
-        setIsLoading(true);
+        setIsTableLoading(true);
         setErrorMessage('');
         try {
             const data = await ServiceFunctions.postRequestMonitoring(
@@ -104,7 +104,7 @@ const RequestMonitoringPage = () => {
             handleShowModal();
         } finally {
             setHasSearched(true);
-            setIsLoading(false);
+            setIsTableLoading(false);
         }
     };
 
@@ -116,7 +116,7 @@ const RequestMonitoringPage = () => {
 
 
 
-            {!hasSearched && !isLoading ? (
+            {!hasSearched && !isTableLoading ? (
 
                 <div className='request-mon-search-wrapper container dash-container d-flex' style={{ flexDirection: "column" }}>
 
@@ -220,7 +220,7 @@ const RequestMonitoringPage = () => {
                         style={{ justifyContent: 'space-between', marginBottom: '9px' }}
                     >
                         <div className='productInfo-price-photo'>
-                            {isLoading && (
+                            {isTableLoading && (
                                 <div
                                     className='d-flex flex-column align-items-center justify-content-center'
                                     style={{ height: '100%', paddingTop: '5%', width: '100%' }}
@@ -228,7 +228,7 @@ const RequestMonitoringPage = () => {
                                     <span className='loader'></span>
                                 </div>
                             )}
-                            {!isLoading && (
+                            {!isTableLoading && (
                                 <>
                                     {/* <div className='productInfo-price-photo-photo'>
                                     <img
@@ -284,7 +284,7 @@ const RequestMonitoringPage = () => {
                         <div
                             className='productInfo-wbInfo'
                         >
-                            {isLoading && (
+                            {isTableLoading && (
                                 <div
                                     className='d-flex flex-column align-items-center justify-content-center'
                                     style={{ height: '100%', paddingTop: '5%', width: '100%' }}
@@ -292,7 +292,7 @@ const RequestMonitoringPage = () => {
                                     <span className='loader'></span>
                                 </div>
                             )}
-                            {!isLoading && (
+                            {!isTableLoading && (
                                 <>
                                     <div className='d-flex'>
                                         <span className='productInfo-wbInfo-rating'>
@@ -357,7 +357,7 @@ const RequestMonitoringPage = () => {
                         </div>
 
                         <div className='barcode-sku-brand'>
-                            {isLoading && (
+                            {isTableLoading && (
                                 <div
                                     className='d-flex flex-column align-items-center justify-content-center'
                                     style={{ height: '100%', paddingTop: '5%', width: '100%' }}
@@ -365,7 +365,7 @@ const RequestMonitoringPage = () => {
                                     <span className='loader'></span>
                                 </div>
                             )}
-                            {!isLoading && (
+                            {!isTableLoading && (
                                 <div className='barcode-wrapper'>
                                     <div className='barcode'>
                                         <div className='barcode-row'>
@@ -397,7 +397,7 @@ const RequestMonitoringPage = () => {
                         <RequestMonitoringFilter setDays={setDays} days={days} />
                     </div>
                     {/* {shouldDisplay ? ( */}
-                    {isLoadingSearch ? (
+                    {isTableLoading ? (
                         <div className="loader-wrapper">
                             <span className="loader"></span>
                         </div>
