209897a5d9930d3cacbeba954446729504ec8fd6
Задорин Артем <artfan95@gmail.com>
2025-03-07 20:24:55 +0700

    admin-blog Правки структуры и стилей навигации для админа

--- src/components/AdminSideNav.jsx
+++ src/components/AdminSideNav.jsx
@@ -9,41 +9,51 @@ const AdminSideNav = () => {
   const chunkArray = url ? url.split('/').reverse() : null;
   const location = chunkArray ? chunkArray[0] : null;
   return (
-    <div className={styles.sideNav}>
+    <div className='side-nav'>
       <div className={styles.sidenavWrapper}>
         <img src={logo} alt='Logo' className={styles.sideNavLogo} />
-        <div className='mt-4'>
-          <p
-            className='sidenav-title submenu-item'
-            onClick={() => navigate('/admin-panel')}
-            style={
-              location === 'admin-panel'
-                ? { fontWeight: 'bold', fontSize: '14px' }
-                : {}
-            }
-          >
-            {location === 'admin-panel' ? (
-              <svg
-                style={{ marginRight: '0.5vw' }}
-                width='8'
-                height='8'
-                viewBox='0 0 8 8'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <circle cx='4' cy='4' r='4' fill='#5329FF' />
-              </svg>
-            ) : null}
-            Админ Панель
-          </p>
-          { false && 
+        <div style={{marginTop: '1.5em'}}>
+            <div
+              className='sidenav-el'
+              onClick={() => navigate('/admin-panel')}
+              style={
+                { fontWeight: location === 'admin-panel' ? 'bold' : 'inherit'}
+              }
+            >
+              <div className='d-flex align-items-center'>
+
+                {location === 'admin-panel' ? (
+                  <svg
+                  className='side-nav-icon'
+                  // style={{ marginRight: '0.5vw' }}
+                  width='8'
+                    height='8'
+                    viewBox='0 0 8 8'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <circle cx='4' cy='4' r='4' fill='#5329FF' />
+                  </svg>
+                ) : null}
+                <span
+                  className='sidenav-title'
+                  style={
+                    location === 'admin-panel'
+                      ? { fontWeight: 'bold', color: 'black' }
+                      : {}
+                    }
+                  >
+
+                  Админ Панель
+                </span>
+              </div>
+            </div>
+          { true && 
           <>
           <p 
             className='sidenav-el sidenav-title submenu-item'
             style={
-              ['blog-list', 'blog-add'].includes(location)
-                ? { fontWeight: 'bold', fontSize: '14px' }
-                : {}
+              { fontWeight: ['blog-list', 'blog-add'].includes(location) ? 'bold' : 'inherit'}
             }
           >
                 Блог список
@@ -53,9 +63,7 @@ const AdminSideNav = () => {
               className='sidenav-title submenu-item'
               onClick={() => navigate('/blog-add')}
               style={
-                'blog-add' === location
-                  ? { fontWeight: 'bold', fontSize: '14px' }
-                  : {}
+                { fontWeight: 'blog-add' === location ? 'bold' : 'inherit'}
               }
             >
               Добавить
@@ -64,9 +72,7 @@ const AdminSideNav = () => {
               className='sidenav-title submenu-item'
               onClick={() => navigate('/blog-list')}
               style={
-                'blog-list' === location
-                  ? { fontWeight: 'bold', fontSize: '14px' }
-                  : {}
+                { fontWeight: 'blog-list' === location ? 'bold' : 'inherit'}
               }
             >
               Список
