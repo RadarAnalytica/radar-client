62b416c0917a5ca346beb74403c64f5f22aff016
Mike Strn <x@pnhd.ru>
2025-03-13 17:17:50 +0300

    wip

--- src/containers/orderMap/OrderMapTable.jsx
+++ src/containers/orderMap/OrderMapTable.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styles from './OrderMapTable.module.css'
 const OrderMapTable = ({ title, data, totalAmount, totalCount }) => {
   const withName = [...data].slice(5);
   // const otherRegion = withName.slice(-5)
@@ -15,36 +15,38 @@ const OrderMapTable = ({ title, data, totalAmount, totalCount }) => {
       <h5 className='fw-bold' style={{ fontSize: '2.5vh' }}>
         {title}
       </h5>
-      <div className='d-flex justify-content-between'>
+      <div className={styles.table}>
         <p className='mb-2 clue-text col-6 pe-2'>Регион</p>
         <p className='mb-2 clue-text col'>Штуки</p>
         <p className='mb-2 clue-text col'>Рубли</p>
         <p className='mb-2 clue-text col text-end'>Доля</p>
-      </div>
       {withName &&
         (withName.length > 5
           ? withName.map((item, key) => (
-            <div key={key} className='d-flex justify-content-between'>
-              <p className='mb-2 col-6 pe-2'>
+          
+            <>
+              <p className={styles.table__rowTitle}>
                 {item.districtName || 'Регион не определен'}
               </p>
               <p className='mb-2 col'>{item.count}&nbsp;шт</p>
-              <p className='mb-2 col'>{item.amount}&nbsp;₽</p>
+              <p className='mb-2 col' style={{ textWrap: 'nowrap'}}>{item.amount}&nbsp;₽</p>
               <p className='mb-2 col text-end fw-bold'>
                 {item.percent.toFixed(1)}&nbsp;%
               </p>
-            </div>
+            </>
+            
           ))
           : withName.map((item, key) => (
-            <div key={key} className='d-flex justify-content-between'>
-              <p className='mb-2 col-6 pe-2'>{item.districtName}</p>
+            <>
+              <p className={styles.table__rowTitle}>{item.districtName}</p>
               <p className='mb-2 col'>{item.count.toFixed(0)}&nbsp;шт</p>
-              <p className='mb-2 col'>{formatNumber(item.amount.toFixed(2))}&nbsp;₽</p>
+              <p className='mb-2 col' style={{ textWrap: 'nowrap'}}>{formatNumber(item.amount.toFixed(2))}&nbsp;₽</p>
               <p className='mb-2 col text-end fw-bold'>
                 {item.percent.toFixed(1)}&nbsp;%
               </p>
-            </div>
+            </>
           )))}
+          </div>
     </div>
   );
 };
