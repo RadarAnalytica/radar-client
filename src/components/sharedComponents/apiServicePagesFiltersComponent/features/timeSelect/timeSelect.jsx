cf433fffe9077d83c04cb2a0e1686cdaae42096a
Mike Strn <x@pnhd.ru>
2025-03-31 20:34:03 +0300

    test-44 done

--- src/components/sharedComponents/apiServicePagesFiltersComponent/features/timeSelect/timeSelect.jsx
+++ src/components/sharedComponents/apiServicePagesFiltersComponent/features/timeSelect/timeSelect.jsx
@@ -9,47 +9,47 @@ import { useAppDispatch, useAppSelector } from '../../../../../redux/hooks';
 import { actions as filtersActions } from '../../../../../redux/apiServicePagesFiltersState/apiServicePagesFilterState.slice';
 import { Select, ConfigProvider } from 'antd'
 
+const predefinedRanges = [
+    {
+        value: 7,
+        title: '7 дней'
+    },
+    {
+        value: 14,
+        title: '14 дней'
+    },
+    {
+        value: 30,
+        title: '30 дней'
+    },
+    {
+        value: 90,
+        title: '90 дней'
+    },
+    {
+        value: 0,
+        title: 'Произвольные даты'
+    }
+];
+
 export const TimeSelect = () => {
 
     const dispatch = useAppDispatch()
     const { selectedRange } = useAppSelector(store => store.filters)
     const [isCalendarOpen, setIsCalendarOpen] = useState(false);
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const [month, setMonth] = useState(new Date());
-    const [selectedOption, setSelectedOption] = useState(selectedRange);
     const [localSelectedRange, setLocalSelectedRange] = useState(selectedRange);
+    const [selectOptions, setSelectOptions] = useState([...predefinedRanges])
+    const [selectValue, setSelectValue] = useState()
 
     const today = new Date();
     const minDate = new Date(today);
-    // const maxDate = new Date(today);
 
     minDate.setDate(today.getDate() - 90);
-    // maxDate.setDate(today.getDate() + 90);
 
     const icon = <SelectIcon />
 
-    const predefinedRanges = [
-        {
-            value: 7,
-            title: '7 дней'
-        },
-        {
-            value: 14,
-            title: '14 дней'
-        },
-        {
-            value: 30,
-            title: '30 дней'
-        },
-        {
-            value: 90,
-            title: '90 дней'
-        },
-        {
-            value: 0,
-            title: 'Произвольные даты'
-        }
-    ];
+
 
     const customRuLocale = {
         ...ru,
@@ -62,24 +62,6 @@ export const TimeSelect = () => {
         },
     };
 
-    const toggleCalendar = () => {
-        setIsCalendarOpen(!isCalendarOpen);
-        setIsDropdownOpen(false);
-    };
-
-    const selectOption = (value) => {
-        if (value === "") {
-            setSelectedOption("");
-            toggleCalendar();
-        } else {
-            setSelectedOption({ period: value });
-            dispatch(filtersActions.setPeriod({ period: value }))
-            //setSelectedRange({period: value});
-            setIsCalendarOpen(false);
-        }
-        setIsDropdownOpen(false);
-    };
-
     const handleDayClick = (day) => {
         if (!localSelectedRange.from || (localSelectedRange.from && localSelectedRange.to)) {
             setLocalSelectedRange({ from: day, to: null });
@@ -89,18 +71,10 @@ export const TimeSelect = () => {
                 ? { from: format(day, 'yyyy-MM-dd'), to: format(from, 'yyyy-MM-dd') }
                 : { from: format(from, 'yyyy-MM-dd'), to: format(day, 'yyyy-MM-dd') };
             setLocalSelectedRange(newRange);
+            setSelectValue(0)
             dispatch(filtersActions.setPeriod(newRange))
-            //setSelectedRange(newRange);
             setIsCalendarOpen(false);
         }
-    };
-
-    const formatDateRange = (range) => {
-        console.log('formatDateRange', range);
-        if (range.from && range.to) {
-            return `${format(range.from, 'dd.MM.yyyy')} - ${format(range.to, 'dd.MM.yyyy')}`;
-        }
-        return "Произвольные даты";
     };
 
     const periodRef = useRef(null);
@@ -108,18 +82,61 @@ export const TimeSelect = () => {
 
     const timeSelectChangeHandler = (value) => {
         if (value !== 0) {
+            setSelectValue(value)
+            setSelectOptions(predefinedRanges)
             dispatch(filtersActions.setPeriod({ period: value }))
         } else {
             setIsCalendarOpen(true)
         }
     }
 
+    useEffect(() => {
+        if (selectValue === undefined) {
+            if (selectedRange.period) {
+                setSelectValue(selectedRange.period)
+            }
+
+            if (selectedRange.from && selectedRange.to) {
+                setSelectValue(0)
+                const newOptions = [...selectOptions].map(_ => {
+                    if (_.value === 0) {
+                        _.title = `${format(selectedRange.from, 'dd.MM.yyyy')} - ${format(selectedRange.to, 'dd.MM.yyyy')}`
+                    }
+
+                    return _
+                })
+                setSelectOptions(newOptions)
+            }
+        }
+    }, [selectValue])
+
+    useEffect(() => {
+        let newOptions = selectOptions;
+        if (selectValue === 0 && !selectedRange.period) {
+            newOptions = [...newOptions].map(_ => {
+                if (_.value === 0) {
+                    _.title = `${format(selectedRange.from, 'dd.MM.yyyy')} - ${format(selectedRange.to, 'dd.MM.yyyy')}`
+                }
+
+                return _
+            })
+        } else {
+            newOptions = [...newOptions].map(_ => {
+                if (_.value === 0) {
+                    _.title = `Произвольные даты`
+                }
+
+                return _
+            })
+        }
+        setSelectOptions(newOptions)
+    }, [selectedRange, selectValue])
+
 
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (periodRef.current && !periodRef.current.contains(event.target)) {
                 setIsCalendarOpen(false);
-                setIsDropdownOpen(false);
             }
         };
 
@@ -161,62 +178,32 @@ export const TimeSelect = () => {
                     <Select
                         suffixIcon={icon}
                         className={styles.select}
-                        options={predefinedRanges.map(i => ({ value: i.value, label: i.title }))}
-                        value={selectedRange.period}
+                        options={[...selectOptions].map(i => ({ value: i.value, label: i.title }))}
+                        value={selectValue}
                         onChange={timeSelectChangeHandler}
                         disabled={isCalendarOpen}
                     />
                 </ConfigProvider>
             </div>
-            {/* <div
-                className={styles.dropdownWrapper}
-                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-            >
-                <div className={styles.selectedOption}>
-                    {selectedOption.period ? `${selectedOption.period} дней` : formatDateRange(localSelectedRange)}
-                </div>
-                <svg
-                    width="14"
-                    height="9"
-                    viewBox="0 0 14 9"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className={styles.arrowIcon}
-                >
-                    <path d="M1 1.5L7 7.5L13 1.5" stroke="#8C8C8C" strokeWidth="2" strokeLinecap="round" />
-                </svg>
-            </div> */}
-            {/* {isDropdownOpen && (
-                <ul className={styles.dropdownMenu}>
-                    {
-                        predefinedRanges.map((el) => <li key={el.value} onClick={() => selectOption(el.value)}>{el.title}</li>)
-                    }
-                    <li onClick={() => selectOption("")} className={styles.customDateOption}>
-                        Произвольные даты
-                    </li>
-                </ul>
-            )} */}
+            <div className={`${styles.calendarPopup} ${isCalendarOpen ? styles.visible : ''}`}>
+                <DayPicker
+                    minDate={minDate}
+                    maxDate={today}
+                    mode="range"
+                    selected={localSelectedRange}
+                    month={month}
+                    onMonthChange={setMonth}
+                    captionLayout="dropdown"
+                    className={styles.customDayPicker}
+                    locale={customRuLocale}
+                    onDayClick={handleDayClick}
+                    disabled={[
+                        { before: minDate },
+                        { after: today },
+                    ]}
+                />
+            </div>
 
-            {isCalendarOpen && (
-                <div className={styles.calendarPopup}>
-                    <DayPicker
-                        minDate={minDate}
-                        maxDate={today}
-                        mode="range"
-                        selected={localSelectedRange}
-                        month={month}
-                        onMonthChange={setMonth}
-                        captionLayout="dropdown"
-                        className={styles.customDayPicker}
-                        locale={customRuLocale}
-                        onDayClick={handleDayClick}
-                        disabled={[
-                            { before: minDate },
-                            { after: today },
-                        ]}
-                    />
-                </div>
-            )}
         </div>
     );
 };
