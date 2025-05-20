52926c02e1b9ff5992ba8998a729eaa189fc1c3b
Mike Strn <x@pnhd.ru>
2025-05-20 19:04:46 +0300

    groups search added

--- src/pages/productsGroupsPages/features/modals/addSkuModal/addSkuModal.jsx
+++ src/pages/productsGroupsPages/features/modals/addSkuModal/addSkuModal.jsx
@@ -2,40 +2,32 @@ import React, { useState, useEffect, useContext } from 'react';
 import styles from './addSkuModal.module.css'
 import { addSkuTableConfig } from '../../../shared';
 import { AddSkuModalFooter } from '../../../entities'
-import { Modal, Checkbox, ConfigProvider, Pagination } from 'antd';
+import { Modal, Checkbox, ConfigProvider, Pagination, Input } from 'antd';
 import wb_icon from '../../../../../assets/wb_icon.png'
 import { URL } from '../../../../../service/config';
 import AuthContext from '../../../../../service/AuthContext';
 
-const mockData = [
-    {
-        product: 'Some product name',
-        brand: 'Some brand name',
-        shop: 'Some shop name',
-        sku: '0001',
-        photo: 'https://basket-16.wbbasket.ru/vol2567/part256714/256714767/images/c246x328/1.webp'
-    },
-    {
-        product: 'Some product name',
-        brand: 'Some brand name',
-        shop: 'Some shop name',
-        sku: '0002',
-        photo: 'https://basket-16.wbbasket.ru/vol2567/part256714/256714767/images/c246x328/1.webp'
-    },
-    {
-        product: 'Some product name',
-        brand: 'Some brand name',
-        shop: 'Some shop name',
-        sku: '0003',
-        photo: 'https://basket-16.wbbasket.ru/vol2567/part256714/256714767/images/c246x328/1.webp'
-    },
-]
+const getFilteredData = (query, data) => {
+    let filteredData = data;
+
+    if (data && query) {
+        filteredData = data.filter((item) =>
+            // item?.sku?.toLowerCase().includes(query.toLowerCase()) ||
+            item?.article?.toLowerCase().includes(query.toLowerCase())
+            // item?.productName?.toLowerCase().includes(query.toLowerCase())
+        );
+    }
+
+    return filteredData;
+}
 
 const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData, getGroupData, initDataFetchingStatus, setDataFetchingStatus, dataFetchingStatus, shops }) => {
     const { authToken } = useContext(AuthContext)
     const [tableData, setTableData] = useState()
+    const [initData, setInitData] = useState()
     const [isDataLoading, setIsDataLoading] = useState(false)
     const [checkedList, setCheckedList] = useState([]);
+    const [searchInputValue, setSearchInputValue] = useState('')
     const [paginationState, setPaginationState] = useState({ current: 1, total: 50, pageSize: 50 });
     const checkAll = tableData && tableData.length === checkedList.length;
     const indeterminate = tableData && checkedList.length > 0 && checkedList.length < tableData.length;
@@ -54,7 +46,7 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
     };
 
     const onCheckAllChange = e => {
-        setCheckedList(e.target.checked ? tableData.map(_ => _.id) : []);
+        setCheckedList(e.target.checked ? tableData.map(_ => _.id) : tableData.filter(_ => _.in_group).map(_ => _.id));
     };
 
     const getProductsList = async (authToken, groupId) => {
@@ -74,6 +66,7 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
             }
             const parsedRes = await res.json();
             setTableData(parsedRes.data.products)
+            setInitData(parsedRes.data.products)
             setPaginationState({ ...paginationState, total: parsedRes.data.products.length })
             setCheckedList(parsedRes.data.products.filter(_ => _.in_group).map(_ => _.id))
             //setGroupData(parsedRes.data)
@@ -117,11 +110,32 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
         setPaginationState({ ...paginationState, current: page })
     }
 
+    const inputKeydownHandler = (e) => {
+        if (e && e.key !== 'Enter') return
+        setTableData(getFilteredData(searchInputValue.trim(), initData))
+    }
+    const searchButtonClickHandler = () => {
+        setTableData(getFilteredData(searchInputValue.trim(), initData))
+    }
+    const inputChangeHandler = (e) => {
+        if (e.target.value === '') {
+            setTableData([...initData])
+        }
+        const regex = /^[a-zA-Zа-яА-Я0-9\s]*$/;
+        if (regex.test(e.target.value)) {
+            setSearchInputValue(e.target.value)
+        }
+    }
+
     useEffect(() => {
         if (groupData && groupData.id) {
             getProductsList(authToken, groupData.id)
         }
     }, [groupData])
+
+    useEffect(() => {
+        setPaginationState({ current: 1, total: tableData?.length, pageSize: 50 })
+    }, [tableData])
 
     return (
         <Modal
@@ -164,7 +178,7 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
                                         /* Рендерим айтем заголовка таблицы с кнопками сортировки (если они нужны) */
                                         return (
 
-                                            <div className={styles.table__headerItem} key={id}>
+                                            <div className={v.hasSearch ? styles.table__headerItem_wide : styles.table__headerItem} key={id}>
 
                                                 {v.hasSelect &&
                                                     <div className={styles.sortControls}>
@@ -185,6 +199,35 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
                                                     </div>
                                                 }
                                                 <p className={styles.table__headerItemTitle}>{v.ruName}</p>
+                                                {v.hasSearch &&
+                                                    <ConfigProvider
+                                                        theme={{
+                                                            token: {
+                                                                colorPrimary: '#5329FF',
+                                                                colorBgContainer: 'transparent'
+                                                            }
+                                                        }}
+                                                    >
+                                                        <Input
+                                                            allowClear
+                                                            value={searchInputValue}
+                                                            onKeyDown={(e) => inputKeydownHandler(e)}
+                                                            onChange={inputChangeHandler}
+                                                            autoCorrect='off'
+                                                            spellCheck={false}
+                                                            autoComplete='off'
+                                                            style={{paddingRight: 0}}
+                                                            suffix={
+                                                                <button className={styles.table__searchButton} onClick={searchButtonClickHandler}>
+                                                                    <svg width="10" height="10" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: '20px', height: '20px'}}>
+                                                                        <path fillRule="evenodd" clipRule="evenodd" d="M1.5 9.60353C1.5 5.25398 5.02601 1.72797 9.37556 1.72797C13.7251 1.72797 17.2511 5.25398 17.2511 9.60353C17.2511 13.9531 13.7251 17.4791 9.37556 17.4791C5.02601 17.4791 1.5 13.9531 1.5 9.60353ZM9.37556 0.227966C4.19758 0.227966 0 4.42555 0 9.60353C0 14.7815 4.19758 18.9791 9.37556 18.9791C11.6946 18.9791 13.8169 18.1371 15.4537 16.7423L19.4834 20.772L20.5441 19.7114L16.5143 15.6816C17.9092 14.0449 18.7511 11.9225 18.7511 9.60353C18.7511 4.42555 14.5535 0.227966 9.37556 0.227966Z" fill="#5329FF" />
+                                                                    </svg>
+
+                                                                </button>
+                                                            }
+                                                        />
+                                                    </ConfigProvider>
+                                                }
                                             </div>
                                         )
                                     })}
@@ -226,7 +269,7 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
                                                 }
 
                                                 return (
-                                                    <div className={styles.table__rowItem} key={id}>
+                                                    <div className={v.hasPhoto ? styles.table__rowItem_wide : styles.table__rowItem} key={id}>
                                                         {v.hasPhoto ?
                                                             <>
                                                                 {v.hasSelect &&
