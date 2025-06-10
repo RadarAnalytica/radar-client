095703ce52cfbb869c9ac4b45686d7cacb7d425e
Mike Strn <x@pnhd.ru>
2025-06-10 12:26:34 +0300

    query and subject changed

--- src/pages/trendingRequestsPage/widgets/tableWidget/config.js
+++ src/pages/trendingRequestsPage/widgets/tableWidget/config.js
@@ -2,12 +2,12 @@ export const tableConfig = [
     {
         //tableName: 'Прочие расходы',
         values: [
-            {ruName: 'Запрос', engName: 'subject', units: null, isSortable: true},
+            {ruName: 'Запрос', engName: 'query', units: null, isSortable: true}, //query
         ]
     },
     {
         values: [
-            {ruName: 'Приоритетный предмет', engName: 'query', units: null, isSortable: false},
+            {ruName: 'Приоритетный предмет', engName: 'subject', units: null, isSortable: false},
             {ruName: 'Частотность за 30 дней, шт', engName: 'frequency', units: null, isSortable: true},
             {ruName: 'Кол-во артикулов по запросу, шт', engName: 'goods_quantity', units: 'шт', isSortable: true},
             {ruName: 'Динамика за 30 дней, %', engName: 'g30', units: '%', isSortable: true},
