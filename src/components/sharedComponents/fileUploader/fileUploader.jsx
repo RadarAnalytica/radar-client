340e6358106e2beca95d7277c93994f719c22c4b
Mike Strn <x@pnhd.ru>
2025-06-20 15:12:32 +0300

    global upload state added

--- src/components/sharedComponents/fileUploader/fileUploader.jsx
+++ src/components/sharedComponents/fileUploader/fileUploader.jsx
@@ -17,14 +17,14 @@ const initUploadStatus = {
 
 const checkFinalStatus = async (token) => {
     try {
-        let res = await fetch(`${URL}/api/file-process?per_page=100`, {
+        let res = await fetch(`${URL}/api/file-process`, {
             headers: {
                 authorization: 'JWT ' + token
             }
         })
 
         if (!res.ok) {
-
+            return
         }
 
         res = await res.json()
@@ -70,7 +70,7 @@ const FileUploader = ({ setShow, setError, getListOfReports }) => {
 
                 const filteredArr = []
                 fileList.forEach(_ => {
-                    const item = finalResult?.find(i => i.original_filename === _.name)
+                    const item = finalResult?.filter(i => i.original_filename === _.name).sort((a,b) => a.id - b.id).pop()
                     if (item) {
                         filteredArr.push(item)
                     }
@@ -80,6 +80,7 @@ const FileUploader = ({ setShow, setError, getListOfReports }) => {
                 await getListOfReports();
                 intervalRef?.current && clearInterval(intervalRef.current)
                 intervalRef.current = null
+                localStorage.removeItem('isFilesUploading')
             }
         } catch {
             setUploadStatus({ ...initUploadStatus, isError: true, message: 'Не удалось загрузить файлы' })
@@ -89,7 +90,8 @@ const FileUploader = ({ setShow, setError, getListOfReports }) => {
     }
 
     const uploadHandler = async () => {
-
+        const filenamesArr = fileList.map(_ => ({name: _.name}))
+        localStorage.setItem('isFilesUploading', JSON.stringify(filenamesArr))
         setUploadStatus({ ...initUploadStatus, isUploading: true })
         setProgressBarState(1)
         const formData = new FormData();
@@ -107,9 +109,9 @@ const FileUploader = ({ setShow, setError, getListOfReports }) => {
 
             if (!response.ok) {
                 setUploadStatus({ ...initUploadStatus, isUploading: false, isError: true, message: response.detail || 'Не удалось загрузить файлы' });
+                localStorage.removeItem('isFilesUploading')
             }
             response = await response.json()
-
             setProgressBarState(2)
             intervalRef.current = setInterval(() => {
                 checkAllUploads(authToken, response.processed_files_count)
@@ -117,6 +119,7 @@ const FileUploader = ({ setShow, setError, getListOfReports }) => {
 
         } catch (error) {
             setUploadStatus({ ...initUploadStatus, isUploading: false, isError: true, message: response.detail || 'Не удалось загрузить файлы' });
+            localStorage.removeItem('isFilesUploading')
         }
     }
 
