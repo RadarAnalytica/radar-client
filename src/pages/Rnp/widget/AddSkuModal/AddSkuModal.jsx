054ba21a0ac65c15680b4cf932ed96d8a35879dc
Задорин Артем <artfan95@gmail.com>
2025-08-01 20:06:04 +0700

    feat: Доработки обновления данных при добавлении артикулов

--- src/pages/Rnp/widget/AddSkuModal/AddSkuModal.jsx
+++ src/pages/Rnp/widget/AddSkuModal/AddSkuModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext, useRef, useCallback, useMemo } from 'react';
 import styles from './addSkuModal.module.css'
 import { addSkuTableConfig } from '../../shared';
 import { AddSkuModalFooter } from '../../entities'
@@ -7,9 +7,10 @@ import wb_icon from '../../../../assets/wbicon.svg';
 import { URL } from '../../../../service/config';
 import AuthContext from '../../../../service/AuthContext';
 import { fetchFilters } from '../../../../redux/apiServicePagesFiltersState/filterActions';
-import { useAppDispatch } from '../../../../redux/hooks';
+import { useAppSelector, useAppDispatch } from '../../../../redux/hooks';
 import { Filters } from '../../../../components/sharedComponents/apiServicePagesFiltersComponent';
 import SkuHeader from '../SkuHeader/SkuHeader';
+import { ServiceFunctions } from '../../../../service/serviceFunctions';
 
 const getFilteredData = (query, data) => {
     let filteredData = data;
@@ -27,206 +28,84 @@ const getFilteredData = (query, data) => {
     return filteredData;
 }
 
-const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData, getGroupData, initDataFetchingStatus, setDataFetchingStatus, dataFetchingStatus, shops, setAlertState, addSku }) => {
+const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData, getGroupData, initDataFetchingStatus, setDataFetchingStatus, dataFetchingStatus, setAlertState, addSku }) => {
     let checkedListRef = useRef(null)
     const scrollContainerRef = useRef(null)
-    const { authToken } = useContext(AuthContext)
     const [tableData, setTableData] = useState()
     const [initData, setInitData] = useState()
     const [isDataLoading, setIsDataLoading] = useState(false)
     const [checkedList, setCheckedList] = useState([]);
-    const [searchInputValue, setSearchInputValue] = useState('')
     const [paginationState, setPaginationState] = useState({ current: 1, total: 50, pageSize: 50 });
     const dispatch = useAppDispatch()
     const checkAll = tableData && tableData.length === checkedList.length;
     const indeterminate = tableData && checkedList.length > 0 && checkedList.length < tableData.length;
+    
+    // 
+    const { authToken } = useContext(AuthContext);
+    const filters = useAppSelector((state) => state.filters);
+    const { activeBrand } = useAppSelector((state) => state.filters);
+    const { shops } = useAppSelector((state) => state.shopsSlice);
 
+    const [skuLoading, setSkuLoading] = useState(true);
+    const [skuList, setSkuList] = useState(null);
 
-    const onCheckboxChange = (e) => {
-        const { value, checked } = e.target;
-        if (checked) {
-            setCheckedList([...checkedList, value])
-            // if (searchInputValue && checkedListRef?.current && checkedListRef?.current.some(_ => _ === value)) {
-            //     let newSavedList = checkedListRef.current;
-            //     const index = newSavedList.findIndex(_ => _ === value);
-            //     checkedListRef.current = newSavedList.splice(index, 1)
-            // }
-        } else {
-            const index = checkedList.findIndex(_ => _ === value)
-            const newList = checkedList
-            newList.splice(index, 1)
-
-            if (searchInputValue && checkedListRef?.current && checkedListRef?.current.some(_ => _ === value)) {
-                let newSavedList = checkedListRef.current;
-                const index = newSavedList.findIndex(_ => _ === value);
-                newSavedList.splice(index, 1)
-                checkedListRef.current = newSavedList
-            }
-            setCheckedList([...newList])
-        }
-    };
-
-    const onCheckAllChange = e => {
-        if (searchInputValue && checkedListRef?.current) {
-            const newSavedList = checkedListRef?.current;
-            tableData.forEach(i => {
-                if (newSavedList.some(_ => _ === i.id)) {
-                    const index = newSavedList.findIndex(_ => _ === i.id);
-                    newSavedList.splice(index, 1)
-                }
-            })
-            checkedListRef.current = newSavedList
-        }
-        setCheckedList(e.target.checked ? tableData.map(_ => _.id) : []);
-    };
-
-    const getProductsList = async (authToken, groupId) => {
-        !tableData && setDataFetchingStatus({ ...initDataFetchingStatus, isLoading: true })
+    const updateSkuList = useCallback(async () => {
+        setSkuLoading(true);
         try {
-            const res = await fetch(`${URL}/api/product/product_groups/${groupId}/products`, {
-                headers: {
-                    'content-type': 'application/json',
-                    'authorization': 'JWT ' + authToken,
-                    'cache': 'no-store'
+            // получение данных по артикулу группы
+            const data = ([
+                {
+                    "id": 1,
+                    "photo": "https://basket-12.wbbasket.ru/vol1735/part173548/173548176/images/c246x328/1.webp",
+                    "title": "гирлянда круглая золото",
+                    "shop": 138,
+                    sku: 123321123321
                 },
-            })
-
-            if (!res.ok) {
-                const parsedData = await res.json()
-                setDataFetchingStatus({ ...initDataFetchingStatus, isError: true, message: parsedData?.detail || 'Что-то пошло не так :(' })
-                return;
-            }
-            const parsedRes = await res.json();
-            setTableData(parsedRes.data.products.sort((a, b) => a.article.localeCompare(b.article)))
-            setInitData(parsedRes.data.products.sort((a, b) => a.article.localeCompare(b.article)))
-            setPaginationState({ ...paginationState, total: parsedRes.data.products.length })
-            setCheckedList(parsedRes.data.products.filter(_ => _.in_group).map(_ => _.id))
-            //setGroupData(parsedRes.data)
-            setDataFetchingStatus(initDataFetchingStatus)
-        } catch {
-            setDataFetchingStatus({ ...initDataFetchingStatus, isError: true, message: 'Что-то пошло не так :(' })
-        }
-    }
-
-    const addProducts = async () => {
-        let requestObject = {
-            name: groupData.name,
-            description: groupData.description,
-            product_ids: checkedList
-        }
-        if (searchInputValue) {
-            let ids = [...checkedList, ...checkedListRef.current]
-            let normalizedIds = []
-            ids.forEach(i => {
-                if (!normalizedIds.some(_ => _ === i)) {
-                    normalizedIds.push(i)
+                {
+                    "id": 2,
+                    "photo": "https://basket-14.wbbasket.ru/vol2154/part215481/215481827/images/c246x328/1.webp",
+                    "title": "гирлянда круглая золото",
+                    "shop": 138,
+                    sku: 123321123321
+                },
+                {
+                    "id": 3,
+                    "photo": null,
+                    "title": "Ремень кожаный для брюк и джинс",
+                    "shop": 138,
+                    sku: 123321123321
                 }
-            })
-
-            requestObject.product_ids = normalizedIds
+            ])
+            setTimeout(() => {
+                setSkuLoading(false);
+                setSkuList(data);
+            }, 2000);
+        } catch(error) {
+            console.error('updateSkuList error', error);
+        } finally {
+            // setSkuLoading(false);
         }
-
-        try {
-            const res = await fetch(`${URL}/api/product/product_groups/${groupData.id}`, {
-                method: 'PATCH',
-                headers: {
-                    'content-type': 'application/json',
-                    'authorization': 'JWT ' + authToken,
-                },
-                body: JSON.stringify(requestObject)
-            })
-
-            if (!res.ok) {
-                const parsedData = await res.json()
-                setDataFetchingStatus({ ...initDataFetchingStatus, isError: true, message: parsedData?.detail || 'Что-то пошло не так :(' })
-                return;
-            }
-            setIsAddSkuModalVisible(false)
-            getGroupData(authToken, groupData.id)
-            setAlertState({ isVisible: true, message: 'Артикул успешно добавлен' })
-            dispatch(fetchFilters(authToken))
-            //setGroupData(parsedRes.data)
-            //setDataFetchingStatus(initDataFetchingStatus)
-        } catch {
-            setDataFetchingStatus({ ...initDataFetchingStatus, isError: true, message: 'Что-то пошло не так :(' })
-        }
-    }
-
-    const paginationHandler = (page) => {
-        setPaginationState({ ...paginationState, current: page })
-    }
-
-    const inputKeydownHandler = (e) => {
-        if (e && e.key !== 'Enter') return
-        searchButtonClickHandler()
-    }
-    const searchButtonClickHandler = () => {
-        const filteredData = getFilteredData(searchInputValue.trim(), initData)
-        const newCheckedList = []
-        checkedList.forEach(i => {
-            if (filteredData.some(_ => _.id === i)) {
-                newCheckedList.push(i)
-            }
-        })
-        // Store current checked items before clearing
-        checkedListRef.current = [...checkedList];
-        setCheckedList(newCheckedList)
-        setTableData(filteredData)
-    }
-    const inputChangeHandler = (e) => {
-        if (e.target.value === '') {
-            setTableData([...initData])
-
-            if (checkedListRef.current) {
-                setCheckedList([...checkedListRef.current, ...checkedList])
-            }
-        }
-        setSearchInputValue(e.target.value)
-    }
+    }, [])
 
     useEffect(() => {
-        if (groupData && groupData.id) {
-            getProductsList(authToken, groupData.id)
+        console.log('activeBrand')
+        if (activeBrand){
+            console.log('activeBrand2')
+            updateSkuList();
         }
-    }, [groupData, groupData.products])
+    }, [isAddSkuModalVisible, activeBrand, shops, filters])
 
-
-
-    useEffect(() => {
-        const paginationNextButton = document.querySelector('.ant-pagination-jump-next')
-        const paginationPrevButton = document.querySelector('.ant-pagination-jump-prev')
-        const paginationSingleNextButton = document.querySelector('.ant-pagination-next')
-        const paginationSinglePrevButton = document.querySelector('.ant-pagination-prev')
-        if (paginationNextButton) {
-         paginationNextButton.setAttribute('title', 'Следующие 5 страниц')
-        }
-        if (paginationSingleNextButton) {
-         paginationSingleNextButton.setAttribute('title', 'Следующая страница')
-        }
-        if (paginationSinglePrevButton) {
-         paginationSinglePrevButton.setAttribute('title', 'Предыдущая страница')
-        }
-        if (paginationPrevButton) {
-         paginationPrevButton.setAttribute('title', 'Предыдущие 5 страниц')
-        }
-     }, [paginationState])
+    // console.log('filters', filters)
+    // console.log('shops', shops)
+    // console.log('activeBrand', activeBrand)
 
     useEffect(() => {
         setPaginationState({ current: 1, total: tableData?.length, pageSize: 50 })
     }, [tableData])
 
-    useEffect(() => {
-        if (!isAddSkuModalVisible) {
-            setSearchInputValue('')
-            setTableData(initData)
-            setCheckedList(initData?.filter(_ => _.in_group).map(_ => _.id));
-        }
-    }, [isAddSkuModalVisible])
-
-    useEffect(() => {
-        const { current } = scrollContainerRef;
-        current?.scrollTo({ top: 0, behavior: 'smooth', duration: 100 })
-    }, [paginationState.current])
+    const paginationHandler = (page) => {
+        console.log('paginationHandler', page)
+    }
 
     return (
         <Modal
@@ -249,63 +128,15 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
                 <div className={styles.modal__header}>
                     <p className={styles.modal__title}>Добавьте артикулы</p>
                 </div>
-                <Filters />
+                <Filters timeSelect={false} />
                 {/* loader */}
-                {dataFetchingStatus.isLoading && !tableData &&
-                    <div className={styles.modal__loaderWrapper}>
-                        <span className='loader'></span>
-                    </div>
-                }
-                {/* main data */}
-                {/* {tableData && */}
-                {true &&
-                    <div className={styles.modal__tableWrapper} ref={scrollContainerRef}>
-
-                    <Flex className={styles.item} gap={20}>
-                        <ConfigProvider
-                            theme={{
-                                token: {
-                                    colorPrimary: '#5329FF',
-                                    colorBgContainer: 'transparent',
-                                    controlInteractiveSize: 20
-                                }
-                            }}
-                        >
-                            <Checkbox
-                                // checked={checkedList.some(_ => _ === product.id)}
-                                // value={product.id}
-                                // onChange={onCheckboxChange}
-                            />
-                        </ConfigProvider>
-                        <SkuHeader />
-                    </Flex>
-                    <Flex className={styles.item} gap={20}>
-                        <ConfigProvider
-                            theme={{
-                                token: {
-                                    colorPrimary: '#5329FF',
-                                    colorBgContainer: 'transparent',
-                                    controlInteractiveSize: 20
-                                }
-                            }}
-                        >
-                            <Checkbox
-                                // checked={checkedList.some(_ => _ === product.id)}
-                                // value={product.id}
-                                // onChange={onCheckboxChange}
-                            />
-                        </ConfigProvider>
-                            <SkuHeader />
-                    </Flex>
-
-
-                    </div>}
                 <ConfigProvider
                     theme={{
                         token: {
                             colorText: '#5329FF',
-                            lineWidth: 0,
-                            colorPrimary: '#5329FF'
+                            colorPrimary: '#5329FF',
+                            colorBgTextHover: '#5329FF0D',
+                            controlInteractiveSize: 20
                         },
                         components: {
                             Pagination: {
@@ -316,14 +147,45 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, groupData,
                         }
                     }}
                 >
+                    {/* {tableData && */}
+                    {/* {!isDataLoading && data.length > 0 && (<div className={styles.modal__tableWrapper}> */}
+                    {skuLoading && <div className={styles.loading}><span className='loader'></span></div>}
+                    {!skuLoading && skuList && (<div className={styles.modal__container}>
+                        {skuList.map((el, i) => (
+                            <Flex key={i} className={styles.item} gap={20}>
+                                <Checkbox
+                                    // checked={checkedList.some(_ => _ === product.id)}
+                                    // value={product.id}
+                                    // onChange={onCheckboxChange}
+                                />
+                                <SkuHeader title={el.title} photo={el.photo} sku={el.sku} shop={shops.find((shop) => (shop.id == el.shop)).brand_name
+                                }/>
+                            </Flex>
+                        ))}
+                    </div>)}
+
                     <Pagination
+                        locale={{
+                            items_per_page: 'записей на странице',
+                            jump_to: 'Перейти',
+                            jump_to_confirm: 'подтвердить',
+                            page: 'Страница',
+                            prev_page: 'Предыдущая страница',
+                            next_page: 'Следующая страница',
+                            prev_5: 'Предыдущие 5 страниц',
+                            next_5: 'Следующие 5 страниц',
+                            prev_3: 'Предыдущие 3 страниц',
+                            next_3: 'Следующие 3 страниц',
+                        }}
                         defaultCurrent={1}
-                        current={paginationState.current}
+                        current={1}
                         onChange={paginationHandler}
-                        total={paginationState.total}
-                        pageSize={paginationState.pageSize}
+                        // total={paginationState.total}
+                        total={0}
+                        // pageSize={paginationState.pageSize}
+                        pageSize={10}
                         showSizeChanger={false}
-                    //showTotal={(total) => `Всего ${total} товаров`}
+                        hideOnSinglePage={true}
                     />
                 </ConfigProvider>
             </div>
--- src/pages/Rnp/widget/SkuHeader/SkuHeader.jsx
+++ src/pages/Rnp/widget/SkuHeader/SkuHeader.jsx
@@ -2,16 +2,12 @@ import {Flex} from 'antd';
 import styles from './SkuHeader.module.css';
 import wb_icon from '../../../../assets/wbicon.svg';
 
-export default function SkuHeader() {
+export default function SkuHeader({ title, photo, sku, shop }) {
 	return (
 		<Flex gap={20}>
-				{true && (
-			<div className={styles.table__rowImgWrapper}>
-				{/* {product[v.photoFieldName] && */}
+			{photo && (<div className={styles.table__rowImgWrapper}>
 					<img
-						src={
-							'https://basket-12.wbbasket.ru/vol1735/part173548/173548176/images/c246x328/1.webp'
-						}
+						src={ photo }
 						width={45}
 						height={60}
 						onError={(e) => {
@@ -19,22 +15,23 @@ export default function SkuHeader() {
 							e.target.style.display = 'none';
 						}}
 					/>
-			</div>
-				)}
+			</div>)}
 			<div className={styles.item__description}>
 				<div className={styles.item__title}>
-					Ремень кожаный для брюк и джинс, резинка в подарок
+					{title}
 				</div>
 				<Flex gap={8} className={styles.item__info} align="center">
-					<Flex gap={8} className={styles.item__article}>
-						<img
-							src={wb_icon}
-							className={styles.item__article_icon}
-						/>
-						Артикул
-					</Flex>
+					<a href={`https://www.wildberries.ru/catalog/${sku}/detail.aspx`} target='_blank'>
+						<Flex gap={8} className={styles.item__article}>
+							<img
+								src={wb_icon}
+								className={styles.item__article_icon}
+							/>
+							Артикул
+						</Flex>
+					</a>
 					&bull;
-					<span className={styles.item__shop}>Название магазина</span>
+					<span className={styles.item__shop}>{shop}</span>
 				</Flex>
 			</div>
 		</Flex>
