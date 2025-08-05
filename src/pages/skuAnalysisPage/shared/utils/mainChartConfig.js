f726fd16eca4b9dbd8834397dbda7052294b9644
Mike Strn <x@pnhd.ru>
2025-08-05 10:34:21 +0300

    test-566 done

--- src/pages/skuAnalysisPage/shared/utils/mainChartConfig.js
+++ src/pages/skuAnalysisPage/shared/utils/mainChartConfig.js
@@ -24,6 +24,30 @@ export const annotationColorsConfig = [
     { bgColor: '#FEEBF0', lableColor: '#F93C65' },
     { bgColor: '#FBEDFF', lableColor: '#D54AFF' },
     { bgColor: '#E5F7ED', lableColor: '#00AF4F' },
+    { bgColor: '#FEEBF4', lableColor: '#F93C94' },
+    { bgColor: '#E8F9FA', lableColor: '#1BC5D1' },
+    { bgColor: '#FEF6EB', lableColor: '#F9A43C' },
+    { bgColor: '#FEEBF0', lableColor: '#F93C65' },
+    { bgColor: '#FBEDFF', lableColor: '#D54AFF' },
+    { bgColor: '#E5F7ED', lableColor: '#00AF4F' },
+    { bgColor: '#FEEBF4', lableColor: '#F93C94' },
+    { bgColor: '#E8F9FA', lableColor: '#1BC5D1' },
+    { bgColor: '#FEF6EB', lableColor: '#F9A43C' },
+    { bgColor: '#FEEBF0', lableColor: '#F93C65' },
+    { bgColor: '#FBEDFF', lableColor: '#D54AFF' },
+    { bgColor: '#E5F7ED', lableColor: '#00AF4F' },
+    { bgColor: '#FEEBF4', lableColor: '#F93C94' },
+    { bgColor: '#E8F9FA', lableColor: '#1BC5D1' },
+    { bgColor: '#FEF6EB', lableColor: '#F9A43C' },
+    { bgColor: '#FEEBF0', lableColor: '#F93C65' },
+    { bgColor: '#FBEDFF', lableColor: '#D54AFF' },
+    { bgColor: '#E5F7ED', lableColor: '#00AF4F' },
+    { bgColor: '#FEEBF4', lableColor: '#F93C94' },
+    { bgColor: '#E8F9FA', lableColor: '#1BC5D1' },
+    { bgColor: '#FEF6EB', lableColor: '#F9A43C' },
+    { bgColor: '#FEEBF0', lableColor: '#F93C65' },
+    { bgColor: '#FBEDFF', lableColor: '#D54AFF' },
+    { bgColor: '#E5F7ED', lableColor: '#00AF4F' },
 ]
 
 const getAnnotations = (initData) => {
@@ -47,7 +71,7 @@ const getAnnotations = (initData) => {
     let annotations = {}
     normilizedData.forEach((i, id) => {
         i.dates.sort((a, b) => moment(a) > moment(b) ? 1 : -1)
-        const color = id < annotationColorsConfig.length - 1 ? annotationColorsConfig[id] : annotationColorsConfig[id - (Math.floor(id / annotationColorsConfig.length - 1) * (annotationColorsConfig.length - 1))]
+        const color = id <= annotationColorsConfig.length - 1 ? annotationColorsConfig[id] : annotationColorsConfig[id - (Math.floor(id / annotationColorsConfig.length - 1) * (annotationColorsConfig.length - 1))]
         annotations[`annotation_${id}`] = {
             drawTime: 'beforeDatasetsDraw',
             type: 'box',
