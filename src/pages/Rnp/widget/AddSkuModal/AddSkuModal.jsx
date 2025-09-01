7d69c227147ad6b2bcaa62798948a58e607c9d2c
Задорин Артем <artfan95@gmail.com>
2025-09-01 17:10:18 +0700

    TEST-742: Изменение зависимостей для запросов

--- src/pages/Rnp/widget/AddSkuModal/AddSkuModal.jsx
+++ src/pages/Rnp/widget/AddSkuModal/AddSkuModal.jsx
@@ -99,7 +99,7 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, addSku }) 
         return () => {
             abortController.abort('Отмена запроса');
         };
-    }, [isAddSkuModalVisible, page, request]);
+    }, [isAddSkuModalVisible, page, request, shops]);
 
     useEffect(() => {
         return () => {
@@ -116,7 +116,7 @@ const AddSkuModal = ({ isAddSkuModalVisible, setIsAddSkuModalVisible, addSku }) 
             setPage(1)
         }
         setRequest((state) => !state);
-    }, [search, filters, activeBrand, shops])
+    }, [search, filters, activeBrand])
 
     return (
         <>
