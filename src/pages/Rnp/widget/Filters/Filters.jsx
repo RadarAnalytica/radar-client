8cf86414932e07da3ab30c2f198b7d5d6399a3c9
Задорин Артем <artfan95@gmail.com>
2025-09-02 13:29:26 +0700

    feat: Доработка обновления фильтров для магазина в статусе сбор данных

--- src/pages/Rnp/widget/Filters/Filters.jsx
+++ src/pages/Rnp/widget/Filters/Filters.jsx
@@ -23,9 +23,9 @@ export const Filters = ({
   const dispatch = useAppDispatch();
   const { activeBrand, shops, filters } = useAppSelector(store => store.filtersRnp);
   // const filters = useAppSelector((state) => state.filtersRnp);
-  // const { messages } = useAppSelector((state) => state.messagesSlice);
-  // const prevMessages = useRef()
   const filtersState = useAppSelector(store => store.filtersRnp)
+  const { messages } = useAppSelector((state) => state.messagesSlice);
+  const prevMessages = useRef()
   //const shops = useAppSelector((state) => state.shopsSlice.shops);
   //--------------------//
 
@@ -93,6 +93,36 @@ export const Filters = ({
     }
   }, [shops])
   //--------------------------------------------------------------------------------//
+
+  //Данные магазина [A-Za-z0-9]+ успешно собраны\. Результаты доступны на страницах сервиса
+  useEffect(() => {
+    // Если это первая пачка сообщений, то данные актуальны и мы просто записываем сообщения для последующего сравнения
+    if (!prevMessages?.current) {
+        prevMessages.current = messages;
+        return
+    }
+    
+    // Если это последующие сообщения ....
+    if (messages && activeBrand?.id === 0 && prevMessages?.current) {
+      // Ищем свежие сообщения
+      let filteredMessages = messages.filter(m => !prevMessages.current.some(_ => _.id === m.id))
+      // Выходим если свежих нет
+      if (!!filteredMessages && filteredMessages.length > 0) {
+        // Если свежие есть, то ищем интересующее нас (про сбор данных магазина) и полученные меньше минуты назад
+        const now = Date.now();
+        filteredMessages = filteredMessages
+          .filter(m => /Данные магазина [A-Za-z0-9]+ успешно собраны\. Результаты доступны на страницах сервиса/.test(m.text))
+          .filter(m => (now - new Date(m.created_at)) < 60000 )
+        
+
+        // Если выходим если таких нет
+        if (!!filteredMessages || filteredMessages.length > 0) {
+          fetchFiltersData();
+        }
+      } 
+    }
+    prevMessages.current = messages
+  }, [messages])
 
   return (
     <div className={styles.filters}>
