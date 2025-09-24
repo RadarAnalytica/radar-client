47a631f08afa5493a5da3cb257a7abfc32c15d87
STRNZ <x@pnhd.ru>
2025-09-24 18:22:05 +0300

    Fix/test 829 (#548)
    * filters Provider + new filter handler ready
    
    * wip
    
    * some upds

--- src/redux/hooks.js
+++ src/redux/hooks.js
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from 'react-redux';
-
 // Typed aliases for TS files; in JS they behave as original hooks
 export const useAppDispatch = () => useDispatch();
 export const useAppSelector = useSelector;
