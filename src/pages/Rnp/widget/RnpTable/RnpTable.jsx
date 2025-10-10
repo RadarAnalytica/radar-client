5a1a13d6a022579d5788fc2f677651f083d2cb8a
Mike Strn <x@pnhd.ru>
2025-10-10 16:39:46 +0300

    rnp table tooltip hidden 2

--- src/pages/Rnp/widget/RnpTable/RnpTable.jsx
+++ src/pages/Rnp/widget/RnpTable/RnpTable.jsx
@@ -17,18 +17,18 @@ const customCellRender = (value, record, index, dataIndex) => {
 	if (dataIndex === 'period' && record.isParent && value === 'Переходы (шт)') {
 		return <div className={styles.customCellBoldTooltip}>
 			<>{value}</>
-				{value === 'Переходы (шт)' &&
-					// <Tooltip
-					// 	title="Отображены только значения из аналитики рекламных кампаний"
-					// 	color="#FFFFFF"
-					// 	overlayInnerStyle={{ color: '#1A1A1A', fontSize: '14px' }}
-					// >
+				{/* {value === 'Переходы (шт)' &&
+					<Tooltip
+						title="Отображены только значения из аналитики рекламных кампаний"
+						color="#FFFFFF"
+						overlayInnerStyle={{ color: '#1A1A1A', fontSize: '14px' }}
+					>
 					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<rect x="0.75" y="0.75" width="18.5" height="18.5" rx="9.25" stroke="black" strokeOpacity="0.1" strokeWidth="1.5" />
 						<path d="M9.064 15V7.958H10.338V15H9.064ZM8.952 6.418V5.046H10.464V6.418H8.952Z" fill="#1A1A1A" fillOpacity="0.5" />
 					</svg>
 				</Tooltip>
-			}
+			} */}
 		</div>;
 	}
 	if (dataIndex === 'period' && !record.isParent) {
