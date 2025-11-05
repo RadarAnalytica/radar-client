ac68f9ca9b89531461536e4593165f9b76d0f5d1
Mike Strn <x@pnhd.ru>
2025-11-05 12:56:17 +0300

    new main page bars (test period and onboarding)

--- src/pages/homePage/MainPage.jsx
+++ src/pages/homePage/MainPage.jsx
@@ -6,10 +6,13 @@ import { VideoWidget, FeaturesWidget, VideoWidgetOneLine } from './widgets';
 import { Banner } from './features';
 import { useDemoMode } from "@/app/providers";
 import NoSubscriptionWarningBlock from '@/components/sharedComponents/noSubscriptionWarningBlock/noSubscriptionWarningBlock';
+import { OnboardingWidget } from '@/widgets';
+import { useContext } from 'react';
+import AuthContext from '@/service/AuthContext';
 
 export default function MainPage() {
     const { isDemoMode } = useDemoMode();
-
+    const { user } = useContext(AuthContext);
     return (
         <main className={styles.page}>
             <MobilePlug />
@@ -23,8 +26,8 @@ export default function MainPage() {
                     <Header title='Главная' />
                 </div>
 
-                {isDemoMode && <NoSubscriptionWarningBlock />}
-
+                {isDemoMode && <NoSubscriptionWarningBlock isOnMainPage />}
+                {!isDemoMode && !user?.is_onboarded && <OnboardingWidget />}
                 <VideoWidgetOneLine />
                 <Banner.Top />
                 <FeaturesWidget />
