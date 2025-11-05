ae1e2e6992287d29823bb1d6f81ba9a5535c4258
Mike Strn <x@pnhd.ru>
2025-11-05 20:29:39 +0300

    cookie issue - attempt to fix

--- src/RouteGuards.tsx
+++ src/RouteGuards.tsx
@@ -144,7 +144,7 @@ export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   const navigate = useNavigate();
   const isCalculateEntryUrl = sessionStorage.getItem('isCalculateEntryUrl');
   const shops = useAppSelector((state) => state.filters.shops);
-  const isSubscribedUser = !user.is_onboarded && ['smart', 'test'].includes(user?.subscription_status?.toLowerCase());
+  const isSubscribedUser = !user?.is_onboarded && ['smart', 'test'].includes(user?.subscription_status?.toLowerCase());
   const isUserHasActiveShop = shops?.some((shop: any) => 
     !shop.is_deleted && 
     (shop.is_valid || shop.is_primary_collect)
