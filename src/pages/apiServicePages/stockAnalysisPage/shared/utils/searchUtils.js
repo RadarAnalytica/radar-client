7cf7170562a8fb79da15f79120b4c80bc44276cc
Mike Strn <x@pnhd.ru>
2025-11-26 13:21:34 +0300

    search updated at SA

--- src/pages/apiServicePages/stockAnalysisPage/shared/utils/searchUtils.js
+++ src/pages/apiServicePages/stockAnalysisPage/shared/utils/searchUtils.js
@@ -1,12 +1,24 @@
 export const getFilteredData = (query, data) => {
+    console.log('SA query', query);
+    console.log('SA data', data);
     let filteredData = data;
 
     if (data && query) {
-        filteredData = data.filter((item) =>
-            item?.sku?.toLowerCase().includes(query.toLowerCase()) ||
-            item?.vendorСode?.toLowerCase().includes(query.toLowerCase()) ||
-            item?.productName?.toLowerCase().includes(query.toLowerCase())
-        );
+        console.log('SA data', data);
+        console.log('SA query', query);
+        if (!query || !data) return [];
+        filteredData = [...data].map((item) => {
+            const newSizes = item?.sizes?.filter((size) => (
+                size?.sku?.toLowerCase().includes(query.toLowerCase()) ||
+                size?.vendorСode?.toLowerCase().includes(query.toLowerCase()) ||
+                size?.productName?.toLowerCase().includes(query.toLowerCase()))
+            )
+
+            return {
+                ...item,
+                sizes: newSizes || [],
+            }
+        }).filter((item) => item.sizes && item.sizes.length > 0)
     }
 
     return filteredData;
