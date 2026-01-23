88f7d53642770f2d9963651a5333833e647205a9
Mike Strn <x@pnhd.ru>
2026-01-23 11:55:38 +0300

    1501 fixed (main branch)

--- src/pages/apiServicePages/stockAnalysisPage/stockAnalysisPage.jsx
+++ src/pages/apiServicePages/stockAnalysisPage/stockAnalysisPage.jsx
@@ -55,7 +55,7 @@ const StockAnalysisPage = () => {
     };
 
     useEffect(() => {
-        if (filters.activeBrand && isFiltersLoaded) {
+        if (activeBrand && isFiltersLoaded && activeBrand.is_primary_collect) {
             fetchAnalysisData();
         }
     }, [isFiltersLoaded, activeBrand, selectedRange, activeBrandName, activeArticle, activeGroup]);
